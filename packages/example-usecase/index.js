2028a9d3e597204167326f7079096dfa331d9321
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-01-29 22:40:19 +0100

    test: Remove unused fixture
    affects: example-usecase

--- packages/example-usecase/index.js
+++ packages/example-usecase/index.js
@@ -32,42 +32,6 @@ class Usecase extends React.Component {
         <Trans className="components">
           Read <a href="/mononoke">more</a>.
         </Trans>
-
-        <Trans className="plural">
-          <Select
-            value={genderOfHost}
-            female={
-              <Plural
-                value={numGuests}
-                offset="1"
-                _0={`${host} does not give a party.`}
-                _1={`${host} invites ${guest} to her party.`}
-                _2={`${host} invites ${guest} and one other person to her party.`}
-                other={`${host} invites ${guest} and # other people to her party.`}
-              />
-            }
-            male={
-              <Plural
-                value={numGuests}
-                offset="1"
-                _0={`${host} does not give a party.`}
-                _1={`${host} invites ${guest} to his party.`}
-                _2={`${host} invites ${guest} and one other person to her party.`}
-                other={`${host} invites ${guest} and # other people to her party.`}
-              />
-            }
-            other={
-              <Plural
-                value={numGuests}
-                offset="1"
-                _0={`${host} does not give a party.`}
-                _1={`${host} invites ${guest} to their party.`}
-                _2={`${host} invites ${guest} and one other person to her party.`}
-                other={`${host} invites ${guest} and # other people to her party.`}
-              />
-            }
-          />
-        </Trans>
       </I18nProvider>
     )
   }
