2518bb309261c83015612276fabc661e47486c3d
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-01-29 16:43:40 +0100

    refactor: Cleanup collecting of trans props
    affects: babel-plugin-transform-react-trans

--- packages/babel-plugin-transform-react-trans/src/index.js
+++ packages/babel-plugin-transform-react-trans/src/index.js
@@ -1,3 +1,15 @@
+function cleanChildren(node) {
+  node.children = []
+  node.openingElement.selfClosing = true
+}
+
+const mergeProps = (props, nextProps) => ({
+  text: props.text + nextProps.text,
+  params: Object.assign({}, props.params, nextProps.params),
+  components: props.components.concat(nextProps.components)
+})
+
+// Plugin function
 export default function({ types: t }) {
   function isIdAttribute(node) {
     return t.isJSXAttribute(node) && t.isJSXIdentifier(node.name, {name: 'id'})
@@ -8,22 +20,19 @@ export default function({ types: t }) {
 
   const isTransElement = isElementFactory('Trans')
 
-  function cleanChildren(node) {
-    node.children = []
-    node.openingElement.selfClosing = true
-  }
-
-  function extract(node) {
-    let text = '',
-      params = {},
-      components = []
+  function extract(node, props) {
+    let nextProps = {
+      text: "",
+      params: {},
+      components: []
+    }
 
     if (t.isJSXExpressionContainer(node)) {
       const exp = node.expression
 
       if (t.isIdentifier(exp)) {
-        text += `{${exp.name}}`
-        params[exp.name] = t.objectProperty(exp, exp)
+        nextProps.text += `{${exp.name}}`
+        nextProps.params[exp.name] = t.objectProperty(exp, exp)
 
       } else if (t.isTemplateLiteral(exp)) {
         let parts = []
@@ -36,49 +45,43 @@ export default function({ types: t }) {
 
         parts.forEach((item) => {
           if (t.isTemplateElement(item)) {
-            text += item.value.raw
+            nextProps.text += item.value.raw
           } else {
-            text += `{${item.name}}`
-            params[item.name] = t.objectProperty(item, item)
+            nextProps.text += `{${item.name}}`
+            nextProps.params[item.name] = t.objectProperty(item, item)
           }
         })
 
       } else {
-        text += exp.value
+        nextProps.text += exp.value
       }
 
     } else if (t.isJSXElement(node)) {
       const index = this.inlineElementCounter++
       const selfClosing = node.openingElement.selfClosing
 
-      text += !selfClosing ? `<${index}>` : `<${index}/>`
+      nextProps.text += !selfClosing ? `<${index}>` : `<${index}/>`
 
       for (const child of node.children) {
-        const {
-          text: newText, params: newParams, components: newComponents
-        } = extract.bind(this)(child)
-
-        text += newText
-        params = Object.assign({}, params, newParams)
-        components = components.concat(newComponents)
+        nextProps = extract.call(this, child, nextProps)
       }
 
-      if (!selfClosing) text += `</${index}>`
+      if (!selfClosing) nextProps.text += `</${index}>`
 
       cleanChildren(node)
-      components.unshift(node)
+      nextProps.components.unshift(node)
 
     } else if (t.isJSXSpreadChild(node)) {
       // TODO: I don't have a clue what's the usecase
 
     } else {
-      text += node.value
+      nextProps.text += node.value
     }
 
     // normalize spaces
-    text = text.replace(/\n\s+/g, "\n")
+    nextProps.text = nextProps.text.replace(/\n\s+/g, "\n")
 
-    return { text, params, components }
+    return mergeProps(props, nextProps)
   }
 
   return {
@@ -92,40 +95,36 @@ export default function({ types: t }) {
 
         // 1. Collect all parameters and inline elements and generate message ID
 
-        let text = "",
-          params = {},
-          components = []
+        let props = {
+          text: "",
+          params: {},
+          components: []
+        }
 
         for (const child of children) {
-          const {
-            text: newText, params: newParams, components: newComponents
-          } = extract.bind(this)(child)
-
-          text += newText
-          params = Object.assign({}, params, newParams)
-          components = components.concat(newComponents)
+          props = extract.call(this, child, props)
         }
 
         // 2. Replace children and add collected data
 
         cleanChildren(node)
-        text = text.trim()
+        props.text = props.text.trim()
 
         // If `id` prop already exists and generated ID is different,
         // add it as a `default` prop
         const idAttr = attrs.filter(isIdAttribute)[0]
-        if (idAttr && idAttr.value.value !== text) {
+        if (idAttr && idAttr.value.value !== props.text) {
           attrs.push(
-            t.JSXAttribute(t.JSXIdentifier("defaults"), t.StringLiteral(text))
+            t.JSXAttribute(t.JSXIdentifier("defaults"), t.StringLiteral(props.text))
           )
         } else if (!idAttr) {
           attrs.push(
-            t.JSXAttribute(t.JSXIdentifier("id"), t.StringLiteral(text))
+            t.JSXAttribute(t.JSXIdentifier("id"), t.StringLiteral(props.text))
           )
         }
 
         // Parameters for variable substitution
-        const paramsList = Object.values(params)
+        const paramsList = Object.values(props.params)
         if (paramsList.length) {
           attrs.push(
             t.JSXAttribute(
@@ -135,15 +134,15 @@ export default function({ types: t }) {
         }
 
         // Inline elements
-        if (components.length) {
+        if (props.components.length) {
           attrs.push(
             t.JSXAttribute(
               t.JSXIdentifier("components"),
-              t.JSXExpressionContainer(t.arrayExpression(components))
+              t.JSXExpressionContainer(t.arrayExpression(props.components))
             )
           )
         }
-      }
-    }
+      }  // JSXElement
+    }  // visitor
   }
 }
