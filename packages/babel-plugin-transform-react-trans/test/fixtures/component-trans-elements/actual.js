01587fca17a91d21fc0c875a1fc935cd6150c0ba
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-01-29 17:59:35 +0100

    test: Test component inside expression containers
    affects: babel-plugin-transform-react-trans

--- packages/babel-plugin-transform-react-trans/test/fixtures/component-trans-elements/actual.js
+++ packages/babel-plugin-transform-react-trans/test/fixtures/component-trans-elements/actual.js
@@ -2,3 +2,5 @@
   Hello <strong>World!</strong><br />
   <p>My name is <a href="/about"><em>{name}</em></a></p>
 </Trans>;
+<Trans>{<span>Component inside expression container</span>}</Trans>;
+<Trans>{<br />}</Trans>;
--- packages/babel-plugin-transform-react-trans/test/fixtures/component-trans-elements/expected.js
+++ packages/babel-plugin-transform-react-trans/test/fixtures/component-trans-elements/expected.js
@@ -1,3 +1,5 @@
 <Trans id="Hello <0>World!</0><1/>\n<2>My name is <3><4>{name}</4></3></2>" params={{
   name: name
 }} components={[<strong />, <br />, <p />, <a href="/about" />, <em />]} />;
+<Trans id="<0>Component inside expression container</0>" components={[<span />]} />;
+<Trans id="<0/>" components={[<br />]} />;
