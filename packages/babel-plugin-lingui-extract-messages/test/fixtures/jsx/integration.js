f7170041831b927ae9451917e12a8da8887a1dfc
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-02-12 17:52:57 +0100

    test: Add tests for all usecases
    affects: babel-plugin-lingui-extract-messages

--- packages/babel-plugin-lingui-extract-messages/test/fixtures/jsx/integration.js
+++ packages/babel-plugin-lingui-extract-messages/test/fixtures/jsx/integration.js
@@ -1,8 +1,21 @@
-<span title={i18n.t`Title`} />;
-<Trans>Hello World</Trans>;
 <Trans>Hi, my name is {name}</Trans>;
-i18n.plural({
+<span title={i18n.t`Title`} />;
+<span title={i18n.plural({
   count: count,
   one: '# book',
   others: '# books'
-})
+})} />;
+
+const a = i18n.t`Title`;
+i18n.t`Title`;
+
+const p = i18n.plural({
+  value: count,
+  one: '# book',
+  others: '# books'
+});
+i18n.plural({
+  value: count,
+  one: '# book',
+  others: '# books'
+});
