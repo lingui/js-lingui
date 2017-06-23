07f7c547cb1763afc76b64e5bff429a2254ca0bb
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-06-23 07:28:01 +0200

    fix: Change imports to match rollup build

--- packages/babel-preset-lingui-react/index.js
+++ packages/babel-preset-lingui-react/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
-    require('babel-plugin-lingui-transform-js').default,
-    require('babel-plugin-lingui-transform-react').default,
-    require('babel-plugin-lingui-extract-messages').default
+    require('babel-plugin-lingui-transform-js'),
+    require('babel-plugin-lingui-transform-react'),
+    require('babel-plugin-lingui-extract-messages')
   ]
 }
