a015afa02f3d8f8de9a86d471bbac3686e42c7f6
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-06-23 11:21:13 +0200

    chore(build): Add custom build step for lingui-cli

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -18,10 +18,19 @@ DIST_DIR=$PACKAGE/lib/
 $NODE_BIN/rimraf $DIST_DIR
 mkdir -p $DIST_DIR
 
-# Compile source using babel
-$NODE_BIN/rollup -c $DIR/rollup.config.js
+CUSTOM_BUILD=$DIR/scripts/prepublish-$PACKAGE_NAME.sh
 
-## Copy source as type definitions
-#find $SRC_DIR -type f -name '*.js' ! -name '*.test.js' -exec sh -c 'cp -f $0 ${0%.js}.js.flow' {} \;
-#rsync -rv --include '*/' --include '*.js.flow' --exclude '*' --prune-empty-dirs $SRC_DIR $DIST_DIR
-#find $SRC_DIR -type f -name '*.js.flow' -delete
+echo $CUSTOM_BUILD
+
+if [ -x $CUSTOM_BUILD ]; then
+    $CUSTOM_BUILD $SRC_DIR $DIST_DIR
+else
+    # Compile source using babel
+    $NODE_BIN/rollup -c $DIR/rollup.config.js
+
+    ## Copy source as type definitions
+    find $SRC_DIR -type f -name '*.js' ! -name '*.test.js' -exec sh -c 'cp -f $0 ${0%.js}.js.flow' {} \;
+    rsync -rv --include '*/' --include '*.js.flow' --exclude '*' --prune-empty-dirs $SRC_DIR $DIST_DIR
+    find $SRC_DIR -type f -name '*.js.flow' -delete
+fi
+
