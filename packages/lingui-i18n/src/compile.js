70a90edbdd7fbd6b801181625efff89baea98a90
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-06-24 20:58:02 +0200

    style: Cleanup and add docstrings

--- packages/lingui-i18n/src/compile.js
+++ packages/lingui-i18n/src/compile.js
@@ -5,15 +5,17 @@ import { compileMessage, loadLanguageData } from './utils.dev'
 
 const isString = s => typeof s === 'string'
 
+const isFunction: isFunctionType = f => typeof f === 'function'
+
 const defaultFormats = (language, languageData = {}, formatStyles = {}) => {
-  const plurals = languageData.plurals
+  const { plurals } = languageData
   const style = format => isString(format)
     ? formatStyles[format] || { style: format }
     : format
 
   const replaceOctothorpe = (value, message) => {
     return ctx => {
-      const msg = typeof message === 'function' ? message(ctx) : message
+      const msg = isFunction(message) ? message(ctx) : message
       const norm = Array.isArray(msg) ? msg : [msg]
       return norm.map(m => isString(m) ? m.replace('#', value) : m)
     }
@@ -44,6 +46,16 @@ const defaultFormats = (language, languageData = {}, formatStyles = {}) => {
 }
 
 // Message -> (Params -> String)
+/**
+ * Compile message in given language and provide language data (e.g: plural)
+ * with formatStyles. The result is a function which takes parameters and return
+ * formatted message.
+ *
+ * @param language     - Language of message
+ * @param message      - Message to parse and compile
+ * @param languageData - Language data (e.g: plurals)
+ * @param formatStyles - Custom format styles
+ */
 export default function compile (
   language: string,
   message: string | Function,
@@ -52,13 +64,12 @@ export default function compile (
 ): (params?: Object) => string {
   let formattedMessage = message
 
-  if (typeof message === 'string') {
+  if (isString(message)) {
     if (process.env.NODE_ENV !== 'production') {
       formattedMessage = /*#__PURE__*/compileMessage(message)
       languageData = /*#__PURE__*/loadLanguageData(language)
     } else {
       // constant message
-      // $FlowIgnore: message is string, we're inside typeof guard
       return (params?: Object) => message
     }
   }
@@ -73,13 +84,23 @@ export default function compile (
 }
 
 // Params -> CTX
+/**
+ * Creates a context object, which formats ICU MessageFormat arguments based on
+ * argument type.
+ *
+ * @param language     - Language of message
+ * @param params       - Parameters for variable interpolation
+ * @param languageData - Language data (e.g: plurals)
+ * @param formatStyles - Custom format styles
+ * @returns {function(string, string, any)}
+ */
 function context ({ language, params, formatStyles, languageData }) {
   const formats = defaultFormats(language, languageData, formatStyles)
 
   const ctx = (name, type, format) => {
     const value = params[name]
     const formatted = formats[type](value, format)
-    const message = typeof formatted === 'function' ? formatted(ctx) : formatted
+    const message = isFunction(formatted) ? formatted(ctx) : formatted
     return Array.isArray(message) ? message.join('') : message
   }
 
