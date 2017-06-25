5db644696c324f6a825530c20b2517d900f60d7f
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-06-25 10:31:51 +0200

    tests: Hide console warnings from deprecated modules

--- packages/lingui-react/src/InjectI18n.test.js
+++ packages/lingui-react/src/InjectI18n.test.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { mount } from 'enzyme'
 
 import { InjectI18n } from '.'
+import { mockConsole } from './mocks'
 
 describe('InjectI18n [DEPRECATED]', function () {
   const context = {
@@ -29,7 +30,8 @@ describe('InjectI18n [DEPRECATED]', function () {
   // Mount HOC(sink) and get the props which were passed from HOC
   const mountHoc = (props = {}, customContext = context) => {
     const { Sink, spy } = sinkFactory()
-    const node = mount(<Sink {...props} />, { context: { i18nManager: customContext } })
+    const node = mount(
+      <Sink {...props} />, { context: { i18nManager: customContext } })
     const receivedProps = spy.mock.calls[spy.mock.calls.length - 1][0]
 
     // Original props are passed with along with i18n prop
@@ -42,36 +44,49 @@ describe('InjectI18n [DEPRECATED]', function () {
   })
 
   it('should pass all props to wrapped component', function () {
-    const props = {
-      foo: 'bar',
-      value: 1
-    }
+    mockConsole(console => {
+      const props = {
+        foo: 'bar',
+        value: 1
+      }
 
-    // eslint-disable-next-line no-unused-vars
-    const { i18n, ...calledProps } = mountHoc(props).props
-    expect(calledProps).toEqual(props)
+      // eslint-disable-next-line no-unused-vars
+      const { i18n, ...calledProps } = mountHoc(props).props
+      expect(calledProps).toEqual(props)
+
+      expect(console.warn).toBeCalledWith(expect.stringContaining('DEPRECATED (removal in 1.x):'))
+    })
   })
 
   it('should provide data from i18n context', function () {
-    const { i18n } = mountHoc().props
-    expect(i18n).toBeDefined()
-    expect(i18n.language).toBeDefined()
-    expect(i18n.messages).toBeDefined()
+    mockConsole(console => {
+      const { i18n } = mountHoc().props
+      expect(i18n).toBeDefined()
+      expect(i18n.language).toBeDefined()
+      expect(i18n.messages).toBeDefined()
+      expect(console.warn).toBeCalledWith(expect.stringContaining('DEPRECATED (removal in 1.x):'))
+    })
   })
 
   it('should subscribe a callback on mount', function () {
-    expect(context.subscribe).not.toBeCalled()
-    mountHoc()
-    expect(context.subscribe).toBeCalled()
-    expect(context.subscribe.mock.calls[0][0]).toBeInstanceOf(Function)
+    mockConsole(console => {
+      expect(context.subscribe).not.toBeCalled()
+      mountHoc()
+      expect(context.subscribe).toBeCalled()
+      expect(context.subscribe.mock.calls[0][0]).toBeInstanceOf(Function)
+      expect(console.warn).toBeCalledWith(expect.stringContaining('DEPRECATED (removal in 1.x):'))
+    })
   })
 
   it('should unsubscribe a callback on unmount', function () {
-    const { node } = mountHoc()
+    mockConsole(console => {
+      const { node } = mountHoc()
 
-    expect(context.unsubscribe).not.toBeCalled()
-    node.unmount()
-    expect(context.unsubscribe).toBeCalled()
-    expect(context.unsubscribe.mock.calls[0][0]).toBeInstanceOf(Function)
+      expect(context.unsubscribe).not.toBeCalled()
+      node.unmount()
+      expect(context.unsubscribe).toBeCalled()
+      expect(context.unsubscribe.mock.calls[0][0]).toBeInstanceOf(Function)
+      expect(console.warn).toBeCalledWith(expect.stringContaining('DEPRECATED (removal in 1.x):'))
+    })
   })
 })
