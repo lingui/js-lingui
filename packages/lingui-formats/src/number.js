52cef84a0142017b4ee1629f776464cdf7248e0f
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-06-29 16:23:27 +0200

    fix: Add flow type def to number format

--- packages/lingui-formats/src/number.js
+++ packages/lingui-formats/src/number.js
@@ -1,8 +1,13 @@
 // @flow
 type NumberFormat = string | {}
 
+type IntlType = {|
+  NumberFormat: Function
+|};
+const globalIntl: IntlType = window.Intl
+
 function number (language: string, format?: NumberFormat = {}): (value: number) => string {
-  const formatter = new Intl.NumberFormat(language, format)
+  const formatter = new globalIntl.NumberFormat(language, format)
   return value => formatter.format(value)
 }
 
