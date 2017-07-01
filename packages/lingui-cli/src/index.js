eca6561e4d9a4cbbfcd46ee8f9522d4450764902
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-07-02 00:09:56 +0200

    fix: Fix package location

--- packages/lingui-cli/src/index.js
+++ packages/lingui-cli/src/index.js
@@ -3,7 +3,7 @@
 const program = require('commander')
 
 program
-  .version(require('./package.json').version)
+  .version(require('../package.json').version)
   .command('add-locale [locales...]', 'Add new locale (generate empty message catalogues for this locale)')
   .command('extract [files...]', 'Extracts messages from source files')
   .command('compile', 'Compile message catalogs')
