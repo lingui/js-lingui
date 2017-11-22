b3b921ed18b7fced2622019b0dc8718dbe0ebd34
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-11-22 23:29:14 +0100

    feat: Add lingui-loader for webpack

--- packages/lingui-loader/src/index.js
+++ packages/lingui-loader/src/index.js
@@ -1,12 +1,26 @@
+import R from 'ramda'
 import getConfig from 'lingui-conf'
-import { compile } from 'lingui-cli/dist/api/compile'
+import { createCompiledCatalog } from 'lingui-cli/dist/api/compile'
 
 export default function (source) {
   const config = getConfig()
   const format = require(`lingui-cli/dist/api/formats/${config.format}`).default(config)
 
-  // source files
-  this.addDependency(headerPath)
+  const locales = format.getLocales()
+  const locale = format.getLocale(this.resourcePath)
 
-  return `export default ${ JSON.stringify(source) }`
+  const catalogs = R.mergeAll(
+    locales.map((locale) => ({ [locale]: format.read(locale) }))
+  )
+
+  const messages = R.mergeAll(
+    Object.keys(catalogs[locale]).map(key => ({
+      [key]: format.getTranslation(catalogs, locale, key, {
+        fallbackLocale: config.fallbackLocale,
+        sourceLocale: config.sourceLocale
+      })
+    }))
+  )
+
+  return createCompiledCatalog(locale, messages)
 }
