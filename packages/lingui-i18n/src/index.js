98ee64495fdec74ae502653fda1ced9b4ccae3c5
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2017-12-31 14:28:06 +0100

    style: Fix linter errors

--- packages/lingui-i18n/src/index.js
+++ packages/lingui-i18n/src/index.js
@@ -2,6 +2,12 @@
 export { i18n, setupI18n } from "./i18n"
 export { date, number } from "./formats"
 
-export type { Catalog, Catalogs, Message, LanguageData, I18n } from "./i18n"
+export type {
+  Catalog,
+  Catalogs,
+  MessageOptions,
+  LanguageData,
+  I18n
+} from "./i18n"
 
 export const i18nMark = (id: string) => id
