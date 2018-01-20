8171a4706f03ada5bfd6d9930f49eb68caaa89f5
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2018-01-20 11:27:52 +0100

    refactor: Split transform-js plugin to methods by node type

--- packages/babel-plugin-transform-js/src/index.js
+++ packages/babel-plugin-transform-js/src/index.js
@@ -36,192 +36,206 @@ export default function({ types: t }) {
     (t.isIdentifier(node.property, { name: "date" }) ||
       t.isIdentifier(node.property, { name: "number" }))
 
-  function processMethod(node, file, props, root = false) {
-    // i18n.t
-    if (isI18nMethod(node)) {
-      if (t.isCallExpression(node.tag)) {
-        const defaults = node.tag.arguments[0]
-        if (!t.isStringLiteral(defaults)) {
-          throw file.buildCodeFrameError(
-            node.tag,
-            "Message ID must be a string"
-          )
-        }
-        processTemplateLiteral(node.quasi, file, props)
-        return {
-          ...props,
-          text: defaults.value,
-          defaults: props.text
-        }
-      } else {
-        processTemplateLiteral(node.quasi, file, props)
-        return props
+  /**
+   * Convert identifiers to *named* arguments and everything else
+   * to *positional* arguments.
+   *
+   * Example:
+   * `world` is named argument and `new Date()` is positional one
+   *
+   * Input:   `Hello ${world}, today is ${new Date()}`
+   * Output:  `Hello {world}, today is {0}`
+   */
+  function expressionToArgument(exp) {
+    const name = t.isIdentifier(exp) ? exp.name : argumentGenerator()
+    const key = t.isIdentifier(exp) ? exp : t.numericLiteral(name)
+
+    return { name, key }
+  }
+
+  function processI18nMethod(node, file, props) {
+    if (t.isCallExpression(node.tag)) {
+      // Message with custom ID, where message is used as defaults
+      // i18n.t('id')`Hello World`
+      const defaults = node.tag.arguments[0]
+      if (!t.isStringLiteral(defaults)) {
+        throw file.buildCodeFrameError(node.tag, "Message ID must be a string")
       }
-
-      // i18n.plural and i18n.select
-    } else if (isChoiceMethod(node.callee)) {
-      const exp = node
-
-      const choices = {}
-      const choicesType = node.callee.property.name.toLowerCase()
-      let variable
-      let offset = ""
-
-      const arg = exp.arguments[0]
-
-      for (const attr of arg.properties) {
-        if (attr.computed) {
-          throw file.buildCodeFrameError(
-            attr,
-            "Computed properties aren't allowed."
-          )
-        }
-
-        const { key } = attr
-        // key is either:
-        // NumericLiteral => convert to `={number}`
-        // StringLiteral => key.value
-        // Literal => key.name
-        const name = t.isNumericLiteral(key)
-          ? `=${key.value}`
-          : key.name || key.value
-
-        if (name === "value") {
-          const exp = attr.value
-          variable = t.isIdentifier(exp) ? exp.name : argumentGenerator()
-          const key = t.isIdentifier(exp) ? exp : t.numericLiteral(variable)
-          props.values[variable] = t.objectProperty(key, exp)
-        } else if (choicesType !== "select" && name === "offset") {
-          // offset is static parameter, so it must be either string or number
-          if (
-            !t.isNumericLiteral(attr.value) &&
-            !t.isStringLiteral(attr.value)
-          ) {
-            throw file.buildCodeFrameError(
-              node.callee,
-              "Offset argument cannot be a variable."
-            )
-          }
-          offset = ` offset:${attr.value.value}`
-        } else {
-          let value = ""
-
-          if (t.isTemplateLiteral(attr.value)) {
-            props = processTemplateLiteral(
-              attr.value,
-              file,
-              Object.assign({}, props, { text: "" })
-            )
-            value = props.text
-          } else if (t.isCallExpression(attr.value)) {
-            props = processMethod(
-              attr.value,
-              file,
-              Object.assign({}, props, { text: "" })
-            )
-            value = props.text
-          } else {
-            value = attr.value.value
-          }
-          choices[name] = value
-        }
+      const newProps = processTemplateLiteral(node.quasi, file, props)
+      return {
+        ...newProps,
+        text: defaults.value,
+        defaults: props.text
       }
-
-      // missing value
-      if (variable === undefined) {
-        throw file.buildCodeFrameError(
-          node.callee,
-          "Value argument is missing."
-        )
-      }
-
-      const choicesKeys = Object.keys(choices)
-
-      // 'other' choice is required
-      if (!choicesKeys.length) {
-        throw file.buildCodeFrameError(
-          node.callee,
-          `Missing ${choicesType} choices. At least fallback argument 'other' is required.`
-        )
-      } else if (!Array.includes(choicesKeys, "other")) {
-        throw file.buildCodeFrameError(
-          node.callee,
-          `Missing fallback argument 'other'.`
-        )
-      }
-
-      // validate plural rules
-      if (choicesType === "plural" || choicesType === "selectordinal") {
-        choicesKeys.forEach(rule => {
-          if (!Array.includes(pluralRules, rule) && !/=\d+/.test(rule)) {
-            throw file.buildCodeFrameError(
-              node.callee,
-              `Invalid plural rule '${rule}'. Must be ${pluralRules.join(
-                ", "
-              )} or exact number depending on your source language ('one' and 'other' for English).`
-            )
-          }
-        })
-      }
-
-      const argument = choicesKeys
-        .map(form => `${form} {${choices[form]}}`)
-        .join(" ")
-      const format = `${variable}, ${choicesType},${offset} ${argument}`
-      props.text = root ? `{${format}}` : format
-    } else if (isFormatMethod(node.callee)) {
-      const exp = node.arguments[0]
-
-      // missing value
-      if (exp === undefined) {
-        throw file.buildCodeFrameError(
-          node.callee,
-          "The first argument of format function must be a variable."
-        )
-      }
-
-      const variable = t.isIdentifier(exp) ? exp.name : argumentGenerator()
-      const key = t.isIdentifier(exp) ? exp : t.numericLiteral(variable)
-
-      const type = node.callee.property.name
-      const parts = [
-        variable, // variable name
-        type // format type
-      ]
-
-      let format = ""
-      const formatArg = node.arguments[1]
-      if (!formatArg) {
-        // Do not throw validation error when format doesn't exist
-      } else if (t.isStringLiteral(formatArg)) {
-        format = formatArg.value
-      } else if (t.isIdentifier(formatArg) || t.isObjectExpression(formatArg)) {
-        if (t.isIdentifier(formatArg)) {
-          format = formatArg.name
-        } else {
-          const formatName = new RegExp(`^${type}\\d+$`)
-          const existing = Object.keys(props.formats).filter(name =>
-            formatName.test(name)
-          )
-          format = `${type}${existing.length || 0}`
-        }
-
-        props.formats[format] = t.objectProperty(
-          t.identifier(format),
-          formatArg
-        )
-      } else {
-        throw file.buildCodeFrameError(
-          formatArg,
-          "Format can be either string for buil-in formats, variable or object for custom defined formats."
-        )
-      }
-
-      if (format) parts.push(format)
-
-      props.values[variable] = t.objectProperty(key, exp)
-      props.text += `${parts.join(",")}`
     }
+
+    // Message is used as the ID
+    // i18n.t`Hello World`
+    return processTemplateLiteral(node.quasi, file, props)
+  }
+
+  function processChoiceMethod(node, file, props, root = false) {
+    const choices = {}
+    const choicesType = node.callee.property.name.toLowerCase()
+    let defaults
+    let variable
+    let offset = ""
+
+    let choiceArguments = node.arguments[0]
+    if (t.isStringLiteral(choiceArguments)) {
+      defaults = choiceArguments.value
+      choiceArguments = node.arguments[1]
+    }
+
+    for (const attr of choiceArguments.properties) {
+      if (attr.computed) {
+        throw file.buildCodeFrameError(
+          attr,
+          "Computed properties aren't allowed."
+        )
+      }
+
+      const { key } = attr
+      // key is either:
+      // NumericLiteral => convert to `={number}`
+      // StringLiteral => key.value
+      // Literal => key.name
+      const name = t.isNumericLiteral(key)
+        ? `=${key.value}`
+        : key.name || key.value
+
+      if (name === "value") {
+        const exp = attr.value
+        const { name, key } = expressionToArgument(exp)
+        variable = name
+        props.values[name] = t.objectProperty(key, exp)
+      } else if (choicesType !== "select" && name === "offset") {
+        // offset is static parameter, so it must be either string or number
+        if (!t.isNumericLiteral(attr.value) && !t.isStringLiteral(attr.value)) {
+          throw file.buildCodeFrameError(
+            node.callee,
+            "Offset argument cannot be a variable."
+          )
+        }
+        offset = ` offset:${attr.value.value}`
+      } else {
+        let value = ""
+
+        if (t.isTemplateLiteral(attr.value)) {
+          props = processTemplateLiteral(attr.value, file, {
+            ...props,
+            text: ""
+          })
+          value = props.text
+        } else if (t.isCallExpression(attr.value)) {
+          props = processMethod(attr.value, file, { ...props, text: "" })
+          value = props.text
+        } else {
+          value = attr.value.value
+        }
+        choices[name] = value
+      }
+    }
+
+    // missing value
+    if (variable === undefined) {
+      throw file.buildCodeFrameError(node.callee, "Value argument is missing.")
+    }
+
+    const choicesKeys = Object.keys(choices)
+
+    // 'other' choice is required
+    if (!choicesKeys.length) {
+      throw file.buildCodeFrameError(
+        node.callee,
+        `Missing ${choicesType} choices. At least fallback argument 'other' is required.`
+      )
+    } else if (!Array.includes(choicesKeys, "other")) {
+      throw file.buildCodeFrameError(
+        node.callee,
+        `Missing fallback argument 'other'.`
+      )
+    }
+
+    // validate plural rules
+    if (choicesType === "plural" || choicesType === "selectordinal") {
+      choicesKeys.forEach(rule => {
+        if (!Array.includes(pluralRules, rule) && !/=\d+/.test(rule)) {
+          throw file.buildCodeFrameError(
+            node.callee,
+            `Invalid plural rule '${rule}'. Must be ${pluralRules.join(
+              ", "
+            )} or exact number depending on your source language ('one' and 'other' for English).`
+          )
+        }
+      })
+    }
+
+    const argument = choicesKeys
+      .map(form => `${form} {${choices[form]}}`)
+      .join(" ")
+    const format = `${variable}, ${choicesType},${offset} ${argument}`
+    props.text = root ? `{${format}}` : format
+
+    if (defaults) {
+      return {
+        ...props,
+        text: defaults,
+        defaults: props.text
+      }
+    }
+
+    return props
+  }
+
+  function processFormatMethod(node, file, props, root) {
+    const exp = node.arguments[0]
+
+    // missing value
+    if (exp === undefined) {
+      throw file.buildCodeFrameError(
+        node.callee,
+        "The first argument of format function must be a variable."
+      )
+    }
+
+    const { name, key } = expressionToArgument(exp)
+
+    const type = node.callee.property.name
+    const parts = [
+      name, // variable name
+      type // format type
+    ]
+
+    let format = ""
+    const formatArg = node.arguments[1]
+    if (!formatArg) {
+      // Do not throw validation error when format doesn't exist
+    } else if (t.isStringLiteral(formatArg)) {
+      format = formatArg.value
+    } else if (t.isIdentifier(formatArg) || t.isObjectExpression(formatArg)) {
+      if (t.isIdentifier(formatArg)) {
+        format = formatArg.name
+      } else {
+        const formatName = new RegExp(`^${type}\\d+$`)
+        const existing = Object.keys(props.formats).filter(name =>
+          formatName.test(name)
+        )
+        format = `${type}${existing.length || 0}`
+      }
+
+      props.formats[format] = t.objectProperty(t.identifier(format), formatArg)
+    } else {
+      throw file.buildCodeFrameError(
+        formatArg,
+        "Format can be either string for buil-in formats, variable or object for custom defined formats."
+      )
+    }
+
+    if (format) parts.push(format)
+
+    props.values[name] = t.objectProperty(key, exp)
+    props.text += `${parts.join(",")}`
 
     return props
   }
@@ -247,16 +261,27 @@ export default function({ types: t }) {
         const { text } = processMethod(item, file, { ...props, text: "" })
         props.text += `{${text}}`
       } else {
-        const name = t.isIdentifier(item) ? item.name : argumentGenerator()
-        const key = t.isIdentifier(item) ? item : t.numericLiteral(name)
+        const { name, key } = expressionToArgument(item)
 
-        // const [name, key] = t.isIdentifier(item)
-        //   ? [item.name, item]
-        //   : [argumentGenerator(), t.numericLiteral(name)]
         props.text += `{${name}}`
         props.values[name] = t.objectProperty(key, item)
       }
     })
+
+    return props
+  }
+
+  function processMethod(node, file, props, root = false) {
+    if (isI18nMethod(node)) {
+      // i18n.t
+      return processI18nMethod(node, file, props)
+    } else if (isChoiceMethod(node.callee)) {
+      // i18n.plural, i18n.select and i18n.selectOrdinal
+      return processChoiceMethod(node, file, props, root)
+    } else if (isFormatMethod(node.callee)) {
+      // i18n.date, i18n.number
+      return processFormatMethod(node, file, props, root)
+    }
 
     return props
   }
