6c7ce0a3b2831fbe8aedeaa66ac85164d566dccf
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2018-04-17 14:44:32 +0200

    fix: make extract work with new i18n._ signature (#206)
    * fix: make extract work with new i18n._ signature
    * fix: update test suite to new api

--- packages/babel-plugin-extract-messages/test/fixtures/js/all.js
+++ packages/babel-plugin-extract-messages/test/fixtures/js/all.js
@@ -2,8 +2,8 @@ _("ignore")
 
 i18n._() // this should be ignored
 i18n._("msg.hello")
-i18n._("msg.default", { defaults: "Hello World" })
-i18n._("msg.default", { defaults: "Hello World" })
+i18n._("msg.default", null, { defaults: "Hello World" })
+i18n._("msg.default", null, { defaults: "Hello World" })
 i18n._("{count, plural, one {# book} other {# books}}", {
   count: count
 })
