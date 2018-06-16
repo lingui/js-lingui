729c2db4540b743121ce36b14586a45b8710416a
huan086 <huan086@gmail.com>
2018-06-16 14:35:41 +0800

    Use typescript library for Typescript extractor, as babel-plugin-transform-typescript only supports very old Typescript version.

--- packages/cli/src/api/extractors/typescript.js
+++ packages/cli/src/api/extractors/typescript.js
@@ -1,10 +1,11 @@
 // @flow
-import { transformFileSync } from "babel-core"
+import fs from "fs"
+import { transform } from "babel-core"
 
-import transformTs from "babel-plugin-transform-typescript"
 import linguiTransformJs from "@lingui/babel-plugin-transform-js"
 import linguiTransformReact from "@lingui/babel-plugin-transform-react"
 import linguiExtractMessages from "@lingui/babel-plugin-extract-messages"
+import * as ts from "typescript"
 
 import type { ExtractorType } from "./types"
 
@@ -16,13 +17,26 @@ const extractor: ExtractorType = {
   },
 
   extract(filename, localeDir) {
-    transformFileSync(filename, {
+    const content = fs.readFileSync(filename, "utf8")
+    const stripped = ts.transpileModule(content, {
+      compilerOptions: {
+        filename: filename,
+        module: ts.ModuleKind.ESNext,
+        target: ts.ScriptTarget.ES2016, // use ES2015 or ES2016 to preserve tagged template literal
+        allowSyntheticDefaultImports: true,
+        jsx: ts.JsxEmit.Preserve, // pass jsx to babel untouched
+        moduleResolution: ts.ModuleResolutionKind.NodeJs
+      }
+    })
+
+    transform(stripped.outputText, {
+      filename,
       plugins: [
         // Plugins run before presets, so we need to import transform-plugins
         // here until we have a better way to run extract-messages plugin
         // *after* all plugins/presets.
         // Transform plugins are idempotent, so they can run twice.
-        transformTs,
+        "syntax-jsx",
         linguiTransformJs,
         linguiTransformReact,
         [linguiExtractMessages, { localeDir }]
