350df2f46d72bbe68bb5aabfe4e4224606363736
huan086 <huan086@gmail.com>
2018-06-20 01:00:50 +0800

    Add unit tests for typescript extractor. Fix typescript extractor not able to parse non-JSX Typescript files.

--- packages/cli/src/api/extractors/typescript.js
+++ packages/cli/src/api/extractors/typescript.js
@@ -18,29 +18,37 @@ const extractor: ExtractorType = {
 
   extract(filename, localeDir) {
     const content = fs.readFileSync(filename, "utf8")
+    const isTsx = filename.endsWith(".tsx")
+    // pass jsx to babel untouched
+    const jsx = isTsx ? ts.JsxEmit.Preserve : ts.JsxEmit.None
     const stripped = ts.transpileModule(content, {
       compilerOptions: {
         filename: filename,
         module: ts.ModuleKind.ESNext,
         target: ts.ScriptTarget.ES2016, // use ES2015 or ES2016 to preserve tagged template literal
         allowSyntheticDefaultImports: true,
-        jsx: ts.JsxEmit.Preserve, // pass jsx to babel untouched
+        jsx: jsx,
         moduleResolution: ts.ModuleResolutionKind.NodeJs
       }
     })
 
+    const plugins = [
+      // Plugins run before presets, so we need to import transform-plugins
+      // here until we have a better way to run extract-messages plugin
+      // *after* all plugins/presets.
+      // Transform plugins are idempotent, so they can run twice.
+      linguiTransformJs,
+      linguiTransformReact,
+      [linguiExtractMessages, { localeDir }]
+    ]
+
+    if (isTsx) {
+      plugins.unshift("syntax-jsx")
+    }
+
     transform(stripped.outputText, {
       filename,
-      plugins: [
-        // Plugins run before presets, so we need to import transform-plugins
-        // here until we have a better way to run extract-messages plugin
-        // *after* all plugins/presets.
-        // Transform plugins are idempotent, so they can run twice.
-        "syntax-jsx",
-        linguiTransformJs,
-        linguiTransformReact,
-        [linguiExtractMessages, { localeDir }]
-      ]
+      plugins
     })
   }
 }
