6bbfa3735c10def79f108e336651a375101d404c
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2018-07-26 12:11:53 +0200

    chore: update release script to publish concurrently

--- scripts/release/release-npm.js
+++ scripts/release/release-npm.js
@@ -1,7 +1,7 @@
 const fs = require("fs-extra")
 const path = require("path")
 const ora = require("ora")
-const { execSync } = require("child_process")
+const { exec } = require("child_process")
 const { getVersion } = require("./version")
 const R = require("ramda")
 
@@ -11,23 +11,40 @@ const argv = require("minimist")(process.argv.slice(2), {
 
 const PACKAGES_DIR = "build/packages"
 
+function execPromise(cmd, options) {
+  return new Promise((resolve, reject) =>
+    exec(cmd, options, error => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve()
+      }
+    })
+  )
+}
+
 async function releasePackage(version, packagePath) {
   const name = packagePath.split("/").reverse()[0]
-  const spinner = ora(`Publishing @lingui/${name}@${version}`)
+  const spinner = ora({
+    isEnabled: false,
+    text: `Publishing @lingui/${name}@${version}`
+  })
 
   const tags = argv.next ? "--tag next" : ""
   const cmd = `npm publish --access public --otp=${argv.otp} ${tags}`
 
-  if (!argv.dryRun) {
-    try {
-      execSync(cmd, {
-        cwd: packagePath
-      })
-    } catch (e) {
-      spinner.fail(`Version ${version} already published!`)
-      return false
-    }
-  } else {
+  spinner.start()
+  try {
+    await execPromise(!argv.dryRun ? cmd : "sleep 1", {
+      cwd: packagePath
+    })
+  } catch (e) {
+    spinner.fail(`Version ${version} already published!`)
+    console.log(e)
+    return false
+  }
+
+  if (argv.dryRun) {
     console.log(`DRY RUN: ${cmd}`)
   }
 
