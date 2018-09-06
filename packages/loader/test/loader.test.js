3ad23f6975a19f0d5530a57005b5b5613c0b7c7f
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2018-09-06 14:39:51 +0200

    test: try to fix problematic async test

--- packages/loader/test/loader.test.js
+++ packages/loader/test/loader.test.js
@@ -4,17 +4,20 @@
 import path from "path"
 import compiler from "./compiler.js"
 
-const skipOnWindows = /^win/.test(process.platform) ? test.skip : test
+const skipOnWindows = /^win/.test(process.platform) ? it.skip : it
 
 describe("lingui-loader", function() {
-  skipOnWindows("should compile catalog", async () => {
+  skipOnWindows("should compile catalog", () => {
     // skip on windows for now
 
-    const stats = await compiler(
-      path.join(".", "locale", "en", "messages.json")
+    expect.assertions(2)
+
+    return compiler(path.join(".", "locale", "en", "messages.json")).then(
+      stats => {
+        const output = stats.toJson()
+        expect(output.errors).toEqual([])
+        expect(output.modules[0].source).toMatchSnapshot()
+      }
     )
-    const output = stats.toJson()
-    expect(output.errors).toEqual([])
-    expect(output.modules[0].source).toMatchSnapshot()
   })
 })
