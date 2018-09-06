bac674120991cb85ec36c31fa136657d8722c189
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2018-09-06 14:47:34 +0200

    test: another attempt to fix timeout error
    Reference:
    https://github.com/facebook/jest/issues/4359

--- packages/loader/test/loader.test.js
+++ packages/loader/test/loader.test.js
@@ -4,20 +4,20 @@
 import path from "path"
 import compiler from "./compiler.js"
 
+jest.useFakeTimers()
+
 const skipOnWindows = /^win/.test(process.platform) ? it.skip : it
 
 describe("lingui-loader", function() {
-  skipOnWindows("should compile catalog", () => {
-    // skip on windows for now
-
+  // skip on windows for now
+  skipOnWindows("should compile catalog", async () => {
     expect.assertions(2)
 
-    return compiler(path.join(".", "locale", "en", "messages.json")).then(
-      stats => {
-        const output = stats.toJson()
-        expect(output.errors).toEqual([])
-        expect(output.modules[0].source).toMatchSnapshot()
-      }
+    const stats = await compiler(
+      path.join(".", "locale", "en", "messages.json")
     )
+    const output = stats.toJson()
+    expect(output.errors).toEqual([])
+    expect(output.modules[0].source).toMatchSnapshot()
   })
 })
