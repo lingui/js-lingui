f2977ad5e4587b0293f8ba498731e86d6a2c6a1f
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2018-09-12 15:36:15 +0200

    feat: migrate lingui config

--- packages/conf/src/index.js
+++ packages/conf/src/index.js
@@ -4,41 +4,89 @@ const cosmiconfig = require("cosmiconfig")
 const { validate } = require("jest-validate")
 const { replacePathSepForRegex } = require("jest-regex-util")
 
-const NODE_MODULES = replacePathSepForRegex(
-  path.sep + "node_modules" + path.sep
-)
+const NODE_MODULES = "node_modules" + path.sep
 
-export function replaceRootDir(conf, rootDir) {
+const pipe = (...functions) => args =>
+  functions.reduce((arg, fn) => fn(arg), args)
+
+export function replaceRootDir(conf, rootDir, keys) {
   const replace = s => s.replace("<rootDir>", rootDir)
-  ;["srcPathDirs", "srcPathIgnorePatterns", "localeDir"].forEach(key => {
-    const value = conf[key]
+  ;(keys || Object.keys(conf)).forEach(originalKey => {
+    const key = replace(originalKey)
+    const value = conf[originalKey]
 
     if (!value) {
     } else if (typeof value === "string") {
       conf[key] = replace(value)
+    } else if (typeof value === "object") {
+      conf[key] = replaceRootDir(conf[originalKey], rootDir)
     } else if (value.length) {
       conf[key] = value.map(replace)
     }
+
+    if (key !== originalKey) delete conf[originalKey]
   })
 
-  conf.rootDir = rootDir
   return conf
 }
 
+/**
+ * Replace fallbackLanguage with fallbackLocale
+ *
+ * Released in lingui-conf 0.9
+ * Remove anytime after 3.x
+ */
+export function fallbackLanguageMigration(config) {
+  const { fallbackLocale, fallbackLanguage, ...newConfig } = config
+
+  newConfig.fallbackLocale = fallbackLocale || fallbackLanguage || ""
+  return newConfig
+}
+
+/**
+ * Replace localeDir, srcPathDirs and srcPathIgnorePatterns with catalogs
+ *
+ * Released in @lingui/conf 3.0
+ * Remove anytime after 4.x
+ */
+export function catalogMigration(config) {
+  const {
+    // These values were default configuration.
+    localeDir = "locale",
+    srcPathDirs = ["<rootDir>"],
+    srcPathIgnorePatterns = [NODE_MODULES],
+    ...newConfig
+  } = config
+
+  if (typeof localeDir === "string") {
+    let newLocaleDir = localeDir
+    if (localeDir.slice(-1) !== path.sep) {
+      newLocaleDir += "/"
+    }
+
+    newConfig.catalogs = {
+      [path.join(newLocaleDir, "{locale}", "messages")]: [].concat(
+        srcPathDirs,
+        srcPathIgnorePatterns.map(pattern => `!${pattern}`)
+      )
+    }
+  }
+
+  return newConfig
+}
+
 export const defaultConfig = {
-  localeDir: "./locale",
-  sourceLocale: "",
-  fallbackLocale: "",
-  pseudoLocale: "",
-  srcPathDirs: ["<rootDir>"],
-  srcPathIgnorePatterns: [NODE_MODULES],
-  format: "po",
-  rootDir: ".",
-  extractBabelOptions: {
-    plugins: [],
-    presets: []
+  catalogs: {
+    [path.join("<rootDir>", "locale", "{locale}", "messages")]:
+      "<rootDir>" + path.sep
   },
-  compileNamespace: "cjs"
+  compileNamespace: "cjs",
+  extractBabelOptions: { plugins: [], presets: [] },
+  fallbackLocale: "",
+  format: "po",
+  locales: [],
+  pseudoLocale: "",
+  sourceLocale: ""
 }
 
 const exampleConfig = {
@@ -63,13 +111,70 @@ const deprecatedConfig = {
     }
     
     Please update your configuration.
+    `,
+  localeDir: config =>
+    ` Option ${chalk.bold(
+      "localeDir"
+    )} is deprecated. Configure source paths using ${chalk.bold(
+      "catalogs"
+    )} instead.
+    
+    @lingui/cli now treats your current configuration as:
+    
+    {
+      ${chalk.bold('"catalogs"')}: ${JSON.stringify(
+      catalogMigration(config).catalogs,
+      null,
+      2
+    )}
+    }
+    
+    Please update your configuration.
+    `,
+  srcPathDirs: config =>
+    ` Option ${chalk.bold(
+      "srcPathDirs"
+    )} is deprecated. Configure source paths using ${chalk.bold(
+      "catalogs"
+    )} instead.
+    
+    @lingui/cli now treats your current configuration as:
+    
+    {
+      ${chalk.bold('"catalogs"')}: ${JSON.stringify(
+      catalogMigration(config).catalogs,
+      null,
+      2
+    )}
+    }
+    
+    Please update your configuration.
+    `,
+  srcPathIgnorePatterns: config =>
+    ` Option ${chalk.bold(
+      "srcPathIgnorePatterns"
+    )} is deprecated. Configure excluded source paths using ${chalk.bold(
+      "catalogs"
+    )} instead.
+    
+    @lingui/cli now treats your current configuration as:
+    
+    {
+      ${chalk.bold('"catalogs"')}: ${JSON.stringify(
+      catalogMigration(config).catalogs,
+      null,
+      2
+    )}
+    }
+    
+    Please update your configuration.
     `
 }
 
-const configValidation = {
+export const configValidation = {
   exampleConfig,
   deprecatedConfig,
-  comment: "See https://lingui.js.org/ref/conf.html for a list of valid options"
+  comment: "Documentation: https://lingui.js.org/ref/conf.html"
 }
 
 export function getConfig({ cwd } = {}) {
@@ -80,9 +185,17 @@ export function getConfig({ cwd } = {}) {
   const config = { ...defaultConfig, ...userConfig }
 
   validate(config, configValidation)
-  // Use deprecated fallbackLanguage, if defined
-  config.fallbackLocale = config.fallbackLocale || config.fallbackLanguage || ""
 
-  const rootDir = result ? path.dirname(result.filepath) : defaultRootDir
-  return replaceRootDir(config, rootDir)
+  if (!config.rootDir) {
+    config.rootDir = result ? path.dirname(result.filepath) : defaultRootDir
+  }
+
+  return pipe(
+    // List config migrations from oldest to newest
+    fallbackLanguageMigration,
+    catalogMigration,
+
+    // `replaceRootDir` should always be the last
+    config => replaceRootDir(config, config.rootDir, ["catalogs"])
+  )(config)
 }
--- packages/conf/src/index.test.js
+++ packages/conf/src/index.test.js
@@ -1,30 +1,96 @@
-import { getConfig, replaceRootDir } from "@lingui/conf"
+import { validate } from "jest-validate"
+import {
+  getConfig,
+  replaceRootDir,
+  catalogMigration,
+  configValidation
+} from "@lingui/conf"
+import { mockConsole, getConsoleMockCalls } from "@lingui/jest-mocks"
 
-describe("lingui-conf", function() {
+describe("@lingui/conf", function() {
   it("should return default config", function() {
     const config = getConfig()
     expect(config).toBeInstanceOf(Object)
-    expect(config.localeDir).toBeDefined()
     expect(config.sourceLocale).toBeDefined()
     expect(config.fallbackLocale).toBeDefined()
     expect(config.pseudoLocale).toBeDefined()
-    expect(config.srcPathDirs).toBeDefined()
-    expect(config.srcPathIgnorePatterns).toBeDefined()
     expect(config.extractBabelOptions).toBeDefined()
+    expect(config).toMatchSnapshot()
+
+    // Deprecated and migrated configuration
+    expect(config.localeDir).not.toBeDefined()
+    expect(config.srcPathDirs).not.toBeDefined()
+    expect(config.srcPathIgnorePatterns).not.toBeDefined()
   })
 
   it("should replace <rootDir>", function() {
     const config = replaceRootDir(
       {
         boolean: false,
-        localeDir: "<rootDir>",
-        srcPathDirs: ["<rootDir>", "rootDir"]
+        catalogs: {
+          "<rootDir>/locale/{locale}/messages": [
+            "<rootDir>/src",
+            "!<rootDir>/ignored"
+          ]
+        }
       },
       "/Root"
     )
 
     expect(config.boolean).toEqual(false)
-    expect(config.localeDir).toEqual("/Root")
-    expect(config.srcPathDirs).toEqual(["/Root", "rootDir"])
+    expect(config.catalogs).toEqual({
+      "/Root/locale/{locale}/messages": ["/Root/src", "!/Root/ignored"]
+    })
+  })
+
+  describe("catalogMigration", function() {
+    it("should show deprecation warning", function() {
+      mockConsole(console => {
+        validate(
+          {
+            localeDir: "./locale",
+            srcPathDirs: ["./src"],
+            srcPathIgnorePatterns: ["/node_modules/"]
+          },
+          configValidation
+        )
+        expect(getConsoleMockCalls(console.warn)).toMatchSnapshot()
+      })
+    })
+
+    it("should provide default config", function() {
+      const config = {}
+
+      expect(catalogMigration(config)).toEqual({
+        catalogs: {
+          "locale/{locale}/messages": ["<rootDir>", "!node_modules/"]
+        }
+      })
+    })
+
+    it("should normalize string localeDir", function() {
+      const config = {
+        localeDir: "./locale"
+      }
+
+      expect(catalogMigration(config)).toEqual({
+        catalogs: {
+          "locale/{locale}/messages": ["<rootDir>", "!node_modules/"]
+        }
+      })
+    })
+    it("should migrate srcPathDirs and srcPathIgnorePatterns", function() {
+      const config = {
+        localeDir: "locale",
+        srcPathDirs: ["src"],
+        srcPathIgnorePatterns: ["src/node_modules/"]
+      }
+
+      expect(catalogMigration(config)).toEqual({
+        catalogs: {
+          "locale/{locale}/messages": ["src", "!src/node_modules/"]
+        }
+      })
+    })
   })
 })
