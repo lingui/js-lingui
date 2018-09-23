5950541391e5edea90b2d3ff6ff91a4ba37a0215
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2018-09-24 01:33:55 +0200

    chore: update dev Babel to v7

--- scripts/build/babel.config.js
+++ scripts/build/babel.config.js
@@ -1,40 +1,33 @@
 // Babel builder doesn't generate CommonJS, but ES modules.
 // Rollup, however, needs `modules: false, babel modules: true`
 module.exports = function(options) {
-  const config = {
-    // Workaround for transform-runtime bug with export * from "./module"
-    // https://github.com/babel/babel/issues/2877#issuecomment-245402025
-    passPerPreset: true,
-    presets: [
-      { plugins: ["transform-runtime"] },
+  const presets = [
+    [
+      "@babel/preset-env",
       {
-        passPerPreset: false,
-        presets: [
-          "react",
-          [
-            "env",
-            {
-              targets: {
-                node: 4,
-                browsers: "> 1%, last 2 versions"
-              },
-              modules: options.modules === false ? false : "commonjs"
-            }
-          ]
-        ]
+        targets: {
+          node: 8,
+          browsers: "> 1%, last 2 versions"
+        },
+        modules: options.modules === false ? false : "commonjs"
       }
     ],
-    plugins: [
-      ["transform-class-properties", { loose: false }],
-      "transform-export-default",
-      "transform-object-rest-spread",
-      "transform-runtime"
-    ]
-  }
+    "@babel/preset-react",
+    "@babel/preset-flow"
+  ]
+
+  const plugins = [
+    ["@babel/plugin-proposal-class-properties", { loose: false }],
+    "@babel/plugin-proposal-export-default-from",
+    "@babel/plugin-transform-runtime"
+  ]
 
   if (process.env.NODE_ENV === "test") {
-    config.plugins.push("transform-es2015-modules-commonjs")
+    plugins.push("@babel/plugin-transform-modules-commonjs")
   }
 
-  return config
+  return {
+    presets,
+    plugins
+  }
 }
