c7fd92361e493c276a042e85b03268c9b8f9ac38
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2018-10-05 17:58:42 +0200

    test: rename test to match package name

--- packages/loader/test/loader.test.js
+++ packages/loader/test/loader.test.js
@@ -8,12 +8,12 @@ jest.useFakeTimers()
 
 const skipOnWindows = /^win/.test(process.platform) ? it.skip : it
 
-describe("lingui-loader", function() {
+describe("@lingui/loader", function() {
   // skip on windows for now
   skipOnWindows("should compile catalog", async () => {
     expect.assertions(2)
 
-    const stats = await compiler(path.join(".", "locale", "en", "messages.po"))
+    const stats = await compiler(path.join("locale", "en", "messages.po"))
     const output = stats.toJson()
     expect(output.errors).toEqual([])
     expect(output.modules[0].source).toMatchSnapshot()
