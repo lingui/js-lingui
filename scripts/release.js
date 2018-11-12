4be72acc94446e815a549586f98e777762e14c68
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2018-11-12 07:17:44 +0100

    chore: commit build stats on release

--- scripts/release.js
+++ scripts/release.js
@@ -100,7 +100,7 @@ async function release() {
   })
   if (!confirmGit) return
 
-  await exec("git add package.json")
+  await exec("git add package.json scripts/build/results.json")
   try {
     await exec(`git commit -m "chore: release v${newVersion}"`)
   } catch (e) {
