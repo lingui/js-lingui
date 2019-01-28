d2bf647da62b7b10d2295e1236581cdf6e425d96
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2019-01-28 10:17:30 +0100

    test: disable mock-fs related tests
    Related issue: https://github.com/tschaub/mock-fs/issues/256

--- packages/cli/src/lingui-init.test.js
+++ packages/cli/src/lingui-init.test.js
@@ -3,119 +3,123 @@ import { installPackages } from "./lingui-init"
 import { mockConsole } from "./mocks"
 import inquirer from "inquirer"
 
-describe("lingui init", function() {
-  let inquirerPrompt
+describe.skip(
+  "mock-fs doesn't work with Node.js +10",
+  "lingui init",
+  function() {
+    let inquirerPrompt
 
-  beforeAll(() => {
-    inquirerPrompt = inquirer.prompt
-    inquirer.prompt = jest.fn()
-  })
-
-  afterAll(() => {
-    inquirer.prompt = inquirerPrompt
-  })
-
-  afterEach(() => {
-    mockFs.restore()
-    inquirer.prompt.mockReset()
-  })
-
-  describe("install packages", function() {
-    beforeEach(() => {
-      inquirer.prompt = jest.fn(prompt =>
-        Promise.resolve({ [prompt.name]: true })
-      )
+    beforeAll(() => {
+      inquirerPrompt = inquirer.prompt
+      inquirer.prompt = jest.fn()
     })
 
-    it("should use yarn when available", () => {
-      mockFs({
-        "yarn.lock": mockFs.file(),
-        "package.json": JSON.stringify({
-          dependencies: {}
-        })
-      })
-
-      expect.assertions(3)
-      return mockConsole(console =>
-        installPackages(true).then(result => {
-          expect(result).toBeTruthy()
-          expect(console.log).toBeCalledWith("yarn add @lingui/core")
-          expect(console.log).toBeCalledWith(
-            "yarn add --dev @lingui/babel-preset-js"
-          )
-        })
-      )
+    afterAll(() => {
+      inquirer.prompt = inquirerPrompt
     })
 
-    it("should use npm when yarn isn't available", () => {
-      mockFs({
-        "package.json": JSON.stringify({
-          dependencies: {}
-        })
+    afterEach(() => {
+      mockFs.restore()
+      inquirer.prompt.mockReset()
+    })
+
+    describe("install packages", function() {
+      beforeEach(() => {
+        inquirer.prompt = jest.fn(prompt =>
+          Promise.resolve({ [prompt.name]: true })
+        )
       })
 
-      expect.assertions(3)
-      return mockConsole(console =>
-        installPackages(true)
-          .then(result => {
+      it("should use yarn when available", () => {
+        mockFs({
+          "yarn.lock": mockFs.file(),
+          "package.json": JSON.stringify({
+            dependencies: {}
+          })
+        })
+
+        expect.assertions(3)
+        return mockConsole(console =>
+          installPackages(true).then(result => {
             expect(result).toBeTruthy()
+            expect(console.log).toBeCalledWith("yarn add @lingui/core")
             expect(console.log).toBeCalledWith(
-              "npm install --save @lingui/core"
-            )
-            expect(console.log).toBeCalledWith(
-              "npm install --save-dev @lingui/babel-preset-js"
+              "yarn add --dev @lingui/babel-preset-js"
             )
           })
-          .catch(error => console.log({ error }))
-      )
-    })
-
-    it("should detect create-react-app project", () => {
-      mockFs({
-        "package.json": JSON.stringify({
-          dependencies: {
-            "react-scripts": "2.0.0"
-          }
-        })
+        )
       })
 
-      expect.assertions(3)
-      return mockConsole(console =>
-        installPackages(true)
-          .then(result => {
-            expect(result).toBeTruthy()
-            expect(console.log).toBeCalledWith(
-              "npm install --save @lingui/react"
-            )
-            expect(console.log).toBeCalledWith(
-              "npm install --save-dev @lingui/babel-preset-react"
-            )
+      it("should use npm when yarn isn't available", () => {
+        mockFs({
+          "package.json": JSON.stringify({
+            dependencies: {}
           })
-          .catch(error => console.log({ error }))
-      )
-    })
-
-    it("should install core only for other projects", () => {
-      mockFs({
-        "package.json": JSON.stringify({
-          dependencies: {}
         })
+
+        expect.assertions(3)
+        return mockConsole(console =>
+          installPackages(true)
+            .then(result => {
+              expect(result).toBeTruthy()
+              expect(console.log).toBeCalledWith(
+                "npm install --save @lingui/core"
+              )
+              expect(console.log).toBeCalledWith(
+                "npm install --save-dev @lingui/babel-preset-js"
+              )
+            })
+            .catch(error => console.log({ error }))
+        )
       })
 
-      expect.assertions(3)
-      return mockConsole(console =>
-        installPackages(true)
-          .then(result => {
-            expect(result).toBeTruthy()
-            expect(console.log).toBeCalledWith(
-              "npm install --save @lingui/core"
-            )
-            expect(console.log).toBeCalledWith(
-              "npm install --save-dev @lingui/babel-preset-js"
-            )
+      it("should detect create-react-app project", () => {
+        mockFs({
+          "package.json": JSON.stringify({
+            dependencies: {
+              "react-scripts": "2.0.0"
+            }
           })
-          .catch(error => console.log({ error }))
-      )
+        })
+
+        expect.assertions(3)
+        return mockConsole(console =>
+          installPackages(true)
+            .then(result => {
+              expect(result).toBeTruthy()
+              expect(console.log).toBeCalledWith(
+                "npm install --save @lingui/react"
+              )
+              expect(console.log).toBeCalledWith(
+                "npm install --save-dev @lingui/babel-preset-react"
+              )
+            })
+            .catch(error => console.log({ error }))
+        )
+      })
+
+      it("should install core only for other projects", () => {
+        mockFs({
+          "package.json": JSON.stringify({
+            dependencies: {}
+          })
+        })
+
+        expect.assertions(3)
+        return mockConsole(console =>
+          installPackages(true)
+            .then(result => {
+              expect(result).toBeTruthy()
+              expect(console.log).toBeCalledWith(
+                "npm install --save @lingui/core"
+              )
+              expect(console.log).toBeCalledWith(
+                "npm install --save-dev @lingui/babel-preset-js"
+              )
+            })
+            .catch(error => console.log({ error }))
+        )
+      })
     })
-  })
-})
+  }
+)
