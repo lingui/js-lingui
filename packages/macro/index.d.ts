4e7d585657ad2176bae958c720a3fa1169517709
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2019-05-17 16:16:39 +0200

    feat: add type definitions for macro

--- packages/macro/index.d.ts
+++ packages/macro/index.d.ts
@@ -1,12 +1,22 @@
 declare module "@lingui/macro" {
   import { MessageDescriptor } from "@lingui/core"
 
-  function t(literals: TemplateStringsArray, ...placeholders: any[]): string
-  function plural(arg: number | string, options): string
-  function selectOrdinal(arg: number | string, options): string
-  function select(arg: string, choices: { [key: string]: string }): string
-  function defineMessages(messages: {[key: string]: MessageDescriptor}): MessageDescriptor
-  function defineMessage(descriptor: MessageDescriptor): MessageDescriptor
+  export function t(
+    literals: TemplateStringsArray,
+    ...placeholders: any[]
+  ): string
+  export function plural(arg: number | string, options: Object): string
+  export function selectOrdinal(arg: number | string, options: Object): string
+  export function select(
+    arg: string,
+    choices: { [key: string]: string }
+  ): string
+  export function defineMessages<
+    M extends { [key: string]: MessageDescriptor }
+  >(messages: M): M
+  export function defineMessage(
+    descriptor: MessageDescriptor
+  ): MessageDescriptor
 
   export const Trans
   export const Plural
