02bbecd8aacd582f497958f7c996caa8183857da
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2019-07-04 13:42:15 +0200

    feat(core): export default i18n object

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,8 @@
 export { setupI18n, I18n, Catalog, Catalogs, Locales } from "./i18n"
 
+// Default i18n object
+import { setupI18n } from "./i18n"
+export const i18n = setupI18n()
+
 import * as formats from "./formats"
 export { formats }
