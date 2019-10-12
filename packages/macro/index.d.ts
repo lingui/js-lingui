4e188904a97c5a679064262ac2a190ac08c391eb
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2019-10-12 19:58:31 +0200

    chore: don't export macro types in namespace

--- packages/macro/index.d.ts
+++ packages/macro/index.d.ts
@@ -2,47 +2,45 @@ import { ComponentType, ReactNode } from "react"
 import { MessageDescriptor } from "@lingui/core"
 import { TransRenderType } from "@lingui/react"
 
-declare namespace LinguiMacro {
-  export function t(
-    literals: TemplateStringsArray,
-    ...placeholders: any[]
-  ): string
-  export function plural(arg: number | string, options: Object): string
-  export function selectOrdinal(arg: number | string, options: Object): string
-  export function select(
-    arg: string,
-    choices: { [key: string]: string }
-  ): string
-  export function defineMessages<
-    M extends { [key: string]: MessageDescriptor }
-  >(messages: M): M
-  export function defineMessage(
-    descriptor: MessageDescriptor
-  ): MessageDescriptor
+export function t(
+  literals: TemplateStringsArray,
+  ...placeholders: any[]
+): string
+export function plural(arg: number | string, options: Object): string
+export function selectOrdinal(arg: number | string, options: Object): string
+export function select(
+  arg: string,
+  choices: { [key: string]: string }
+): string
+export function defineMessages<
+  M extends { [key: string]: MessageDescriptor }
+>(messages: M): M
+export function defineMessage(
+  descriptor: MessageDescriptor
+): MessageDescriptor
 
-  export interface TransProps {
-    id?: string
-    comment?: string
-    render?: TransRenderType
-  }
-
-  export interface ChoiceProps extends TransProps {
-    value?: string | number
-    offset?: number
-    zero: ReactNode
-    one: ReactNode
-    few: ReactNode
-    many: ReactNode
-    other: ReactNode
-  }
-
-  export interface SelectProps extends TransProps {
-    value?: string
-    other?: ReactNode
-  }
-
-  export const Trans: ComponentType<TransProps>
-  export const Plural: ComponentType<ChoiceProps>
-  export const Select: ComponentType<SelectProps>
-  export const SelectOrdinal: ComponentType<ChoiceProps>
+export interface TransProps {
+  id?: string
+  comment?: string
+  render?: TransRenderType
 }
+
+export interface ChoiceProps extends TransProps {
+  value?: string | number
+  offset?: number
+  zero: ReactNode
+  one: ReactNode
+  few: ReactNode
+  many: ReactNode
+  other: ReactNode
+}
+
+export interface SelectProps extends TransProps {
+  value?: string
+  other?: ReactNode
+}
+
+export const Trans: ComponentType<TransProps>
+export const Plural: ComponentType<ChoiceProps>
+export const Select: ComponentType<SelectProps>
+export const SelectOrdinal: ComponentType<ChoiceProps>
