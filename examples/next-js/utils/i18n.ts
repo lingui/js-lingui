a6cca42dca9847b35e924eb9b8ae400cbd56e011
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2020-04-04 23:12:46 +0200

    chore: remove await from sync calls

--- examples/next-js/utils/i18n.ts
+++ examples/next-js/utils/i18n.ts
@@ -2,6 +2,6 @@ import { i18n } from "@lingui/core"
 
 export async function activate(locale) {
   const catalog = await import(`../locale/${locale}/messages.js`)
-  await i18n.load(locale, catalog)
-  await i18n.activate(locale)
+  i18n.load(locale, catalog, true)
+  i18n.activate(locale)
 }
