fd7433bde57f6c516e1107ca3cd4f862c78bc52e
Tomáš Ehrlich <tomas.ehrlich@gmail.com>
2020-04-04 22:45:55 +0200

    docs: fix re-rendering in Next.js example

--- examples/next-js/pages/_app.tsx
+++ examples/next-js/pages/_app.tsx
@@ -1,8 +1,7 @@
-import { useEffect } from "react"
+import { Fragment, useEffect } from "react"
 
-import { I18nProvider } from "@lingui/react"
+import { I18nProvider, useLingui } from "@lingui/react"
 import { i18n } from "@lingui/core"
-
 import { activate } from "../utils/i18n"
 
 export default function Page({ Component, pageProps }) {
@@ -11,9 +10,21 @@ export default function Page({ Component, pageProps }) {
   }, [])
 
   return (
-    // @ts-ignore
     <I18nProvider i18n={i18n}>
-      <Component {...pageProps} />
+      <I18nWatchLocale>
+        <Component {...pageProps} />
+      </I18nWatchLocale>
     </I18nProvider>
   )
+}
+
+function I18nWatchLocale({ children }) {
+  const { i18n } = useLingui()
+
+  // Skip render when locale isn't loaded
+  if (!i18n.locale) return null
+
+  // Force re-render when locale changes. Otherwise string translations (e.g.
+  // t`Macro`) won't be updated.
+  return <Fragment key={i18n.locale}>{children}</Fragment>
 }
