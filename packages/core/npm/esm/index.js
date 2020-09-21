7f73b772e7819754e10f9eaec289358ee19ec03e
Laszlo Balogh <sharon0x1@gmail.com>
2020-09-21 09:06:52 +0100

    feat: add an ES module wrapper (#737)
    With this wrapper, a Node.js based ES module can use the `named import`syntax. E.g. `import {i18n} ...`

---
+++ packages/core/npm/esm/index.js
@@ -0,0 +1,6 @@
+import i18nModule from '../index.js'
+
+export const setupI18n = i18nModule.setupI18n
+export const I18n = i18nModule.I18n
+export const i18n = i18nModule.i18n
+export const formats = i18nModule.formats
