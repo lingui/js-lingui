4ad90d4c60b752e9f0d82f3dd71936711c51573d
Sergio Moreno <sergiomorenoalbert@gmail.com>
2021-01-06 22:09:24 +0100

    fix(detect-locale): ESM import not published (#926)

---
+++ packages/detect-locale/npm/esm/index.js
@@ -0,0 +1,11 @@
+import detectLocaleModule from '../index.js'
+
+export const detect = detectLocaleModule.detect
+export const multipleDetect = detectLocaleModule.multipleDetect
+export const fromCookie = detectLocaleModule.fromCookie
+export const fromHtmlTag = detectLocaleModule.fromHtmlTag
+export const fromNavigator = detectLocaleModule.fromNavigator
+export const fromPath = detectLocaleModule.fromPath
+export const fromStorage = detectLocaleModule.fromStorage
+export const fromSubdomain = detectLocaleModule.fromSubdomain
+export const fromUrl = detectLocaleModule.fromUrl
