9990ebaa9d30f7e218c106a2abfd7ddbcf0e0170
Sergio Moreno <sergiomorenoalbert@gmail.com>
2021-05-18 18:10:11 +0200

    fix: reduce size of ESM packages (#1066)

--- packages/react/npm/esm/index.js
+++ packages/react/npm/esm/index.js
@@ -1,12 +1,1 @@
-import {
-  I18nProvider as devI18nProvider, Trans as devTrans, useLingui as devuseLingui, withI18n as devwithI18n
-} from "./react.development.js"
-
-import {
-  I18nProvider as I18nProviderProd, Trans as TransProd, useLingui as useLinguiProd, withI18n as withI18nProd
-} from "./react.production.min.js"
-
-export const I18nProvider = process.env.NODE_ENV === "production" ? I18nProviderProd : devI18nProvider;
-export const Trans = process.env.NODE_ENV === "production" ? TransProd : devTrans;
-export const useLingui = process.env.NODE_ENV === "production" ? useLinguiProd : devuseLingui;
-export const withI18n = process.env.NODE_ENV === "production" ? withI18nProd : devwithI18n;
+export * from "./react.production.min";
