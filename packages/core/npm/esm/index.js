3a057e0c61224b98c93203e0d88136fa48f309ba
Sergio Moreno <sergiomorenoalbert@gmail.com>
2021-05-18 18:10:11 +0200

    fix: reverts reduce size of ESM packages (#1066)
    This reverts commit 9990ebaa9d30f7e218c106a2abfd7ddbcf0e0170.

--- packages/core/npm/esm/index.js
+++ packages/core/npm/esm/index.js
@@ -1,1 +1,18 @@
-export * from './core.production.min';
+import {
+    i18n as i18nProd,
+    setupI18n as setupI18nProd,
+    formats as formatsProd,
+    I18n as I18nProd
+} from './core.production.min';
+
+import {
+    i18n as i18nDev,
+    setupI18n as setupI18nDev,
+    formats as formatsDev,
+    I18n as I18nDev
+} from './core.development';
+
+export const i18n = process.env.NODE_ENV === 'production' ? i18nProd : i18nDev;
+export const setupI18n = process.env.NODE_ENV === 'production' ? setupI18nProd : setupI18nDev;
+export const formats = process.env.NODE_ENV === 'production' ? formatsProd : formatsDev;
+export const I18n = process.env.NODE_ENV === 'production' ? I18nProd : I18nDev;
