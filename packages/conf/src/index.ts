6b2d6622b715e95b914b14df17501cc486ec45f4
Jérôme Steunou <jerome.steunou+github@gmail.com>
2021-09-27 18:37:57 +0200

    fix: Add missing babel properties to config validation (#1135)

--- packages/conf/src/index.ts
+++ packages/conf/src/index.ts
@@ -183,6 +183,10 @@ const exampleConfig = {
     rootMode: "rootmode",
     plugins: ["plugin"],
     presets: ["preset"],
+    targets: multipleValidOptions({}, '> 0.5%', ['> 0.5%', 'not dead'], undefined),
+    assumptions: multipleValidOptions({}, undefined),
+    browserslistConfigFile: multipleValidOptions(true, undefined),
+    browserslistEnv: multipleValidOptions('.browserslistrc', undefined),
   },
 }
 
