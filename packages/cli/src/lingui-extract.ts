a41cf84f43dfa6c330a8c85154ad7be723bade8d
Michaël Hoste <michael.hoste@gmail.com>
2021-12-07 15:26:16 +0100

    Fix service provider importation on case-sensitive filesystems

--- packages/cli/src/lingui-extract.ts
+++ packages/cli/src/lingui-extract.ts
@@ -73,7 +73,9 @@ export default async function command(
 
   // If service key is present in configuration, synchronize with cloud translation platform
   if (typeof config.service === 'object' && config.service.name && config.service.name.length) {
-    import(`./services/${config.service.name}`)
+    const serviceFileName = config.service.name.charAt(0).toLowerCase() + config.service.name.slice(1);
+
+    import(`./services/${serviceFileName}`)
       .then(module => module.default(config, options))
       .catch(err => console.error(`Can't load service module ${config.service.name}`, err))
   }
