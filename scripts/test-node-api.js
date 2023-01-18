d3fa47d596b4af907d5c1631d8c5f424b243df17
Timofei Iatsenko <ty@time.sc>
2023-01-18 22:40:48 +0100

    fix(build) explicitly expose api/extractors

---
+++ scripts/test-node-api.js
@@ -0,0 +1,7 @@
+const assert = require("assert")
+assert(require.resolve('@lingui/cli/api'));
+assert(require.resolve('@lingui/cli/api/extractors/typescript.js'));
+assert(require.resolve('@lingui/cli/api/extractors/babel.js'));
+
+assert(require.resolve('@lingui/core'));
+assert(require.resolve('@lingui/core/compile'));
