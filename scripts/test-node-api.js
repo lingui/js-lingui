35071b1c550c442b99e2b18b82ca611f1234e9cf
Martin Chrástek <chrastek12@gmail.com>
2023-01-19 13:57:39 +0100

    Apply suggestions from code review

--- scripts/test-node-api.js
+++ scripts/test-node-api.js
@@ -1,7 +1,7 @@
 const assert = require("assert")
 assert(require.resolve('@lingui/cli/api'));
-assert(require.resolve('@lingui/cli/api/extractors/typescript.js'));
-assert(require.resolve('@lingui/cli/api/extractors/babel.js'));
+assert(require.resolve('@lingui/cli/api/extractors/typescript'));
+assert(require.resolve('@lingui/cli/api/extractors/babel'));
 
 assert(require.resolve('@lingui/core'));
 assert(require.resolve('@lingui/core/compile'));
