66af09b07096d876a3c167a64acb68fb52dc538d
Timofey <mask04@list.ru>
2023-01-19 13:07:29 +0100

    Update packages/core/compile.js
    Co-authored-by: Martin Chrástek <chrastek12@gmail.com>

--- packages/core/compile.js
+++ packages/core/compile.js
@@ -1,5 +1,5 @@
 /**
- * This entry for old runtimes which is not support `exports` field in package.json
+ * This entry is for old runtimes which do not support `exports` field in package.json
  * https://github.com/facebook/metro/issues/670
  */
 module.exports = require("./build/cjs/compile.js")
