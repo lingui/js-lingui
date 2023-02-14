5dd50d34152e7eb393437e5de3d35b3baf09e861
Tao Zhou <121891361+taozhou-glean@users.noreply.github.com>
2023-02-14 05:40:56 -0800

    fix: chain extract on watched file changes (#1435)

--- packages/cli/src/lingui-extract.ts
+++ packages/cli/src/lingui-extract.ts
@@ -176,9 +176,14 @@ if (require.main === module) {
 
   const changedPaths = new Set<string>()
   let debounceTimer: NodeJS.Timer
+  let previousExtract = Promise.resolve(true)
   const dispatchExtract = (filePath?: string[]) => {
-    // Skip debouncing if not enabled
-    if (!program.debounce) return extract(filePath)
+    // Skip debouncing if not enabled but still chain them so no racing issue
+    // on deleting the tmp folder.
+    if (!program.debounce) {
+      previousExtract = previousExtract.then(() => extract(filePath))
+      return previousExtract
+    }
 
     filePath?.forEach((path) => changedPaths.add(path))
 
