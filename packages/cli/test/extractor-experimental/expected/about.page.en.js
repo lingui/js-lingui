7fc9dee5387f209fde62757055fd8bd73f7062f6
Timofei Iatsenko <ty@time.sc>
2023-03-15 11:42:38 +0100

    test(experimental-extractor): update snapshots for new default message sorting (#1520)

--- packages/cli/test/extractor-experimental/expected/about.page.en.js
+++ packages/cli/test/extractor-experimental/expected/about.page.en.js
@@ -1,1 +1,1 @@
-/*eslint-disable*/module.exports={messages:JSON.parse("{\"LGGfGX\":\"header message\",\"u5PTM8\":\"about page message\"}")};
+/*eslint-disable*/module.exports={messages:JSON.parse("{\"u5PTM8\":\"about page message\",\"LGGfGX\":\"header message\"}")};
--- packages/cli/test/extractor-experimental/expected/about.page.pl.js
+++ packages/cli/test/extractor-experimental/expected/about.page.pl.js
@@ -1,1 +1,1 @@
-/*eslint-disable*/module.exports={messages:JSON.parse("{\"LGGfGX\":\"translation: header message\",\"u5PTM8\":\"about page message\",\"nPi9F1\":\"this should be marked as obsolete\"}")};
+/*eslint-disable*/module.exports={messages:JSON.parse("{\"u5PTM8\":\"about page message\",\"LGGfGX\":\"translation: header message\",\"nPi9F1\":\"this should be marked as obsolete\"}")};
