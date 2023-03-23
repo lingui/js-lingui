e22abb40f6e7ae095179a1bc1a9a3c1fed6a84c1
Timofei Iatsenko <ty@time.sc>
2023-03-23 15:44:43 +0100

    docs(): add vite-react-babel example

---
+++ examples/vite-project-react-babel/src/i18n.ts
@@ -0,0 +1,11 @@
+import { i18n } from "@lingui/core"
+
+/**
+ * Load messages for requested locale and activate it.
+ * This function isn't part of the LinguiJS library because there are
+ * many ways how to load messages — from REST API, from file, from cache, etc.
+ */
+export async function loadCatalog(locale: string) {
+  const catalog = await import(`./locales/${locale}.po`)
+  i18n.loadAndActivate(locale, catalog.messages)
+}
