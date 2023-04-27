d84bb8ce05e74e8b62be95afa0af422545b04c55
Timofei Iatsenko <ty@road.travel>
2023-04-27 14:46:04 +0200

    feat(config): show more clear error message when config is not found (#1606)

--- packages/conf/src/getConfig.ts
+++ packages/conf/src/getConfig.ts
@@ -4,6 +4,7 @@ import { cosmiconfigSync, LoaderSync } from "cosmiconfig"
 import path from "path"
 import { makeConfig } from "./makeConfig"
 import type { JITIOptions } from "jiti/dist/types"
+import chalk from "chalk"
 
 function configExists(path: string) {
   return path && fs.existsSync(path)
@@ -19,6 +20,29 @@ function JitiLoader(): LoaderSync {
   }
 }
 
+const moduleName = "lingui"
+
+const configExplorer = cosmiconfigSync(moduleName, {
+  searchPlaces: [
+    `${moduleName}.config.js`,
+    `${moduleName}.config.cjs`,
+    `${moduleName}.config.ts`,
+    `${moduleName}.config.mjs`,
+    "package.json",
+    `.${moduleName}rc`,
+    `.${moduleName}rc.json`,
+    `.${moduleName}rc.yaml`,
+    `.${moduleName}rc.yml`,
+    `.${moduleName}rc.ts`,
+    `.${moduleName}rc.js`,
+  ],
+  loaders: {
+    ".js": JitiLoader(),
+    ".ts": JitiLoader(),
+    ".mjs": JitiLoader(),
+  },
+})
+
 export function getConfig({
   cwd,
   configPath,
@@ -29,34 +53,27 @@ export function getConfig({
   skipValidation?: boolean
 } = {}): LinguiConfigNormalized {
   const defaultRootDir = cwd || process.cwd()
-  const moduleName = "lingui"
-
-  const configExplorer = cosmiconfigSync(moduleName, {
-    searchPlaces: [
-      `${moduleName}.config.js`,
-      `${moduleName}.config.cjs`,
-      `${moduleName}.config.ts`,
-      `${moduleName}.config.mjs`,
-      "package.json",
-      `.${moduleName}rc`,
-      `.${moduleName}rc.json`,
-      `.${moduleName}rc.yaml`,
-      `.${moduleName}rc.yml`,
-      `.${moduleName}rc.ts`,
-      `.${moduleName}rc.js`,
-    ],
-    loaders: {
-      ".js": JitiLoader(),
-      ".ts": JitiLoader(),
-      ".mjs": JitiLoader(),
-    },
-  })
 
   configPath = configPath || process.env.LINGUI_CONFIG
 
   const result = configExists(configPath)
     ? configExplorer.load(configPath)
     : configExplorer.search(defaultRootDir)
+
+  if (!result) {
+    console.error("Lingui was unable to find a config!\n")
+    console.error(
+      `Create ${chalk.bold(
+        "'lingui.config.js'"
+      )} file with LinguiJS configuration in root of your project (next to package.json). See ${chalk.underline(
+        "https://lingui.dev/ref/conf"
+      )}`
+    )
+
+    // gracefully stop further executing
+    throw new Error("No Config")
+  }
+
   const userConfig = result ? result.config : {}
 
   return makeConfig(
