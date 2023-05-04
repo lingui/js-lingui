a7487159e89e6e94e51993e43eaa5e8af341d7b0
Vojtech Novak <vonovak@gmail.com>
2023-05-04 09:37:42 +0200

    chore: reformat

--- examples/react-native/src/PaddedButton.tsx
+++ examples/react-native/src/PaddedButton.tsx
@@ -1,24 +1,24 @@
 import {
-	Button as RNButton,
-	StyleSheet,
-	View,
-	Platform,
-	ButtonProps,
+  Button as RNButton,
+  StyleSheet,
+  View,
+  Platform,
+  ButtonProps,
 } from "react-native";
 import React from "react";
 
 export const Button = (props: ButtonProps) => (
-	<View style={styles.margin}>
-		<RNButton {...props} />
-	</View>
+  <View style={styles.margin}>
+    <RNButton {...props} />
+  </View>
 );
 
 const styles = StyleSheet.create({
-	margin: {
-		...Platform.select({
-			android: {
-				margin: 10,
-			},
-		}),
-	},
+  margin: {
+    ...Platform.select({
+      android: {
+        margin: 10,
+      },
+    }),
+  },
 });
