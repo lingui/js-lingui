9f9c7674dfd61a510157fc0f99d1e64a5970d057
Timofei Iatsenko <ty@road.travel>
2023-05-11 10:30:21 +0200

    fix(message-utils): workaround package.json exports field

---
+++ packages/message-utils/generateMessageId.js
@@ -0,0 +1,6 @@
+/**
+ * this file is a workaround for older runtimes
+ * such as React Native or Jest 27 which is not
+ * support package.json exports field
+ **/
+exports = require("./dist/generateMessageId.cjs")
