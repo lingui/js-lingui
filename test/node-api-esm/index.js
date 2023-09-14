8f4efe42ad24b770f405dd789f2cd714e1b17a46
Timofei Iatsenko <ty@road.travel>
2023-09-14 09:40:47 +0200

    feat(react): export react server sub-entry

--- test/node-api-esm/index.js
+++ test/node-api-esm/index.js
@@ -6,3 +6,4 @@ assert(await import("@lingui/cli/api/extractors/typescript"))
 assert(await import("@lingui/cli/api/extractors/babel"))
 
 assert(await import("@lingui/core"))
+assert(await import("@lingui/react/server"))
