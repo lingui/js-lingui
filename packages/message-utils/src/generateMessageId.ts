a0d8d6ee3301103b17391efcfc9f84259d18d242
Timofei Iatsenko <ty@road.travel>
2023-10-03 12:02:53 +0200

    feat(message-utils): make generateMessageId to be working in browser / lambda / etc

--- packages/message-utils/src/generateMessageId.ts
+++ packages/message-utils/src/generateMessageId.ts
@@ -1,11 +1,17 @@
-import crypto from "crypto"
+import { sha256 } from "js-sha256"
 
 const UNIT_SEPARATOR = "\u001F"
 
 export function generateMessageId(msg: string, context = "") {
-  return crypto
-    .createHash("sha256")
-    .update(msg + UNIT_SEPARATOR + (context || ""))
-    .digest("base64")
-    .slice(0, 6)
+  return hexToBase64(sha256(msg + UNIT_SEPARATOR + (context || ""))).slice(0, 6)
+}
+
+function hexToBase64(hexStr: string) {
+  let base64 = ""
+  for (let i = 0; i < hexStr.length; i++) {
+    base64 += !((i - 1) & 1)
+      ? String.fromCharCode(parseInt(hexStr.substring(i - 1, i + 1), 16))
+      : ""
+  }
+  return btoa(base64)
 }
