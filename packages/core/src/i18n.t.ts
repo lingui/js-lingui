c8cf4d1fb4b2b55a9e54d20efa39a90dc27fc027
bowzee <dilame.bowzee@gmail.com>
2024-01-24 01:07:22 +0800

    chore(core/i18n.t/values): write explanation comments at each line of type ExtractVars

--- packages/core/src/i18n.t.ts
+++ packages/core/src/i18n.t.ts
@@ -69,25 +69,29 @@ type FormatterInputType<FormatterId extends string> =
       : never
     : never
 
-type ExtractVars<Input extends string> = string extends Input
-  ? Record<string, unknown>
-  : Input extends `${string}{${infer Tail}`
-  ? ExtractBraceBody<Tail> extends [
+type ExtractVars<Input extends string> = string extends Input // Check if the type of Input is the general 'string' type and not a string literal.
+  ? Record<string, unknown> // If it is, return a wide type Record<string, unknown> to represent any object.
+  : // Start processing the literal string.
+  Input extends `${string}{${infer Tail}` // Check if the Input string contains '{', indicating the start of an expression.
+  ? // Extract the content inside the curly braces and the rest of the string after the closing curly brace.
+    ExtractBraceBody<Tail> extends [
       infer BraceBody extends string,
       infer Next extends string
     ]
-    ? BraceBody extends `${infer FormatterInput},${infer FormatterTail}`
-      ?
-          | {
+    ? BraceBody extends `${infer FormatterInput},${infer FormatterTail}` // Check if the content inside the braces is a formatter.
+      ? // Process the formatter content.
+        | {
+              // Create a type with a property where the key is trimmed FormatterInput (which is var name in this case), and the value is determined by the formatter's type.
               [P in Trim<FormatterInput>]: FormatterInputType<
                 ExtractFormatterId<FormatterTail>
               >
             }
-          | ExtractVars<ExtractFormatterMessages<FormatterTail>[number]>
-          | ExtractVars<Next>
-      : { [P in Trim<BraceBody>]: string } | ExtractVars<Next>
-    : {}
-  : {}
+          | ExtractVars<ExtractFormatterMessages<FormatterTail>[number]> // Recursively process messages inside the formatter.
+          | ExtractVars<Next> // Recursively process the rest of the string after the current variable.
+      : // If not a formatter, create a type with a property where the key is the whole trimmed BraceBody and the value is a string
+        { [P in Trim<BraceBody>]: string } | ExtractVars<Next> // and recursively process the rest of the string.
+    : {} // If the string does not contain a valid structure, return an empty object type (possibly we can return an error here, because this branch indicates parsing error)
+  : {} // If the Input string does not contain '{', return an empty object type.
 
 export type I18nTValues<Input extends string> = Simplify<
   UnionToIntersection<ExtractVars<DropEscapedBraces<Input>>>
