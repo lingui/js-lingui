13f9ccbe0195b571c87109f4f4fe8d5bb1f1bff9
Timofei Iatsenko <ty@road.travel>
2024-04-03 09:02:35 +0200

    refactor(macro): explicitly mark symbols as deprecated (#1898)

--- packages/macro/index.d.ts
+++ packages/macro/index.d.ts
@@ -1,8 +1,94 @@
+import {
+  t as _t,
+  plural as _plural,
+  defineMessage as _defineMessage,
+  msg as _msg,
+  select as _select,
+  selectOrdinal as _selectOrdinal,
+  SelectOptions as _SelectOptions,
+  ChoiceOptions as _ChoiceOptions,
+  MacroMessageDescriptor as _MacroMessageDescriptor,
+} from "@lingui/core/macro"
+
+import {
+  Trans as _Trans,
+  Plural as _Plural,
+  Select as _Select,
+  SelectOrdinal as _SelectOrdinal,
+  PluralChoiceProps as _PluralChoiceProps,
+  SelectChoiceProps as _SelectChoiceProps,
+  CommonProps as _CommonProps,
+  TransProps as _TransProps,
+  useLingui as _useLingui,
+} from "@lingui/react/macro"
+
 /**
- * @deprecated please use `@lingui/core/macro` directly
+ * @deprecated please import from `@lingui/core/macro` directly
  */
-export * from "@lingui/core/macro"
+const t = _t
 /**
- * @deprecated please use `@lingui/react/macro` directly
+ * @deprecated please import from `@lingui/core/macro` directly
  */
-export * from "@lingui/react/macro"
+const msg = _msg
+/**
+ * @deprecated please import from `@lingui/core/macro` directly
+ */
+const plural = _plural
+/**
+ * @deprecated please import from `@lingui/core/macro` directly
+ */
+const defineMessage = _defineMessage
+/**
+ * @deprecated please import from `@lingui/core/macro` directly
+ */
+const select = _select
+/**
+ * @deprecated please import from `@lingui/core/macro` directly
+ */
+const selectOrdinal = _selectOrdinal
+/**
+ * @deprecated please import from `@lingui/core/macro` directly
+ */
+const SelectOptions = _SelectOptions
+/**
+ * @deprecated please import from `@lingui/core/macro` directly
+ */
+const ChoiceOptions = _ChoiceOptions
+/**
+ * @deprecated please import from `@lingui/core/macro` directly
+ */
+const MacroMessageDescriptor = _MacroMessageDescriptor
+
+export const Trans = _Trans
+/**
+ * @deprecated please import from `@lingui/react/macro` directly
+ */
+export const Plural = _Plural
+/**
+ * @deprecated please import from `@lingui/react/macro` directly
+ */
+export const PluralChoiceProps = _PluralChoiceProps
+/**
+ * @deprecated please import from `@lingui/react/macro` directly
+ */
+export const SelectChoiceProps = _SelectChoiceProps
+/**
+ * @deprecated please import from `@lingui/react/macro` directly
+ */
+export const Select = _Select
+/**
+ * @deprecated please import from `@lingui/react/macro` directly
+ */
+export const CommonProps = _CommonProps
+/**
+ * @deprecated please import from `@lingui/react/macro` directly
+ */
+export const SelectOrdinal = _SelectOrdinal
+/**
+ * @deprecated please import from `@lingui/react/macro` directly
+ */
+export const TransProps = _TransProps
+/**
+ * @deprecated please import from `@lingui/react/macro` directly
+ */
+export const useLingui = _useLingui
