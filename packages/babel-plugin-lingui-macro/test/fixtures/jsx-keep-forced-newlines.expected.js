afc962cd5dfbe1ae01d05fffe7381a577d55bfcc
Timofei Iatsenko <ty@road.travel>
2024-04-12 08:22:47 +0200

    chore(macro): snapshot testing (#1912)

--- packages/babel-plugin-lingui-macro/test/fixtures/jsx-keep-forced-newlines.expected.js
+++ packages/babel-plugin-lingui-macro/test/fixtures/jsx-keep-forced-newlines.expected.js
@@ -1,2 +1,2 @@
 import { Trans as _Trans } from "@lingui/react"
-;<_Trans id={"<stripped>"} message={"Keep multiple\nforced\nnewlines!"} />
+;<_Trans id={"9xE5pD"} message={"Keep multiple\nforced\nnewlines!"} />
