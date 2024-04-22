d27bbe7966da0b1474968912ab13618114df339a
Aseer KT <aseermiqdad@gmail.com>
2024-04-22 13:16:52 +0530

    chore: format rspack config file

--- examples/rspack/rspack.config.js
+++ examples/rspack/rspack.config.js
@@ -1,53 +1,53 @@
-const rspack = require("@rspack/core");
+const rspack = require("@rspack/core")
 
 /**
  * @type {import('@rspack/cli').Configuration}
  */
 module.exports = {
-	context: __dirname,
-	entry: {
-		main: "./src/main.tsx"
-	},
-	resolve: {
-		extensions: [".js", ".ts", ".tsx", ".jsx"]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svg$/,
-				type: "asset"
-			},
-			{
-				test: /\.(jsx?|tsx?)$/,
-                exclude: /node_modules/,
-				use: [
-					{
-						loader: "builtin:swc-loader",
-						options: {
-							sourceMap: true,
-							jsc: {
-								parser: {
-									syntax: "typescript",
-									tsx: true
-								},
-                                experimental: {
-                                    plugins: [['@lingui/swc-plugin', {}]]
-                                },
-								transform: {
-									react: {
-										runtime: "automatic",
-									}
-								}
-							},
-						}
-					}
-				]
-			}
-		]
-	},
-	plugins: [
-		new rspack.HtmlRspackPlugin({
-			template: "./index.html"
-		}),
-	]
-};
+  context: __dirname,
+  entry: {
+    main: "./src/main.tsx",
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: "asset",
+      },
+      {
+        test: /\.(jsx?|tsx?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "builtin:swc-loader",
+            options: {
+              sourceMap: true,
+              jsc: {
+                parser: {
+                  syntax: "typescript",
+                  tsx: true,
+                },
+                experimental: {
+                  plugins: [["@lingui/swc-plugin", {}]],
+                },
+                transform: {
+                  react: {
+                    runtime: "automatic",
+                  },
+                },
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      template: "./index.html",
+    }),
+  ],
+}
