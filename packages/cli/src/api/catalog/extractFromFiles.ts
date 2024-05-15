8ea3ed356baf91eecdafbd4b24fefdcf93ecfbaa
Aseer KT <aseermiqdad@gmail.com>
2024-05-15 10:12:45 +0530

    fix: default message extraction

--- packages/cli/src/api/catalog/extractFromFiles.ts
+++ packages/cli/src/api/catalog/extractFromFiles.ts
@@ -3,11 +3,11 @@ import type {
   ExtractorType,
   LinguiConfigNormalized,
 } from "@lingui/conf"
-import extract from "../extractors"
-import path from "path"
 import chalk from "chalk"
+import path from "path"
+import extract from "../extractors"
+import { ExtractedCatalogType, MessageOrigin } from "../types"
 import { prettyOrigin } from "../utils"
-import { MessageOrigin, ExtractedCatalogType } from "../types"
 
 export async function extractFromFiles(
   paths: string[],
@@ -50,6 +50,7 @@ export async function extractFromFiles(
 
         messages[next.id] = {
           ...prev,
+          message: prev.message ?? next.message,
           comments: next.comment
             ? [...prev.comments, next.comment]
             : prev.comments,
