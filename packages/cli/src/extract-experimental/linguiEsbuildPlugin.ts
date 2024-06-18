8b35acf775ddd3c16c8e4827a390cd59d54b3f1e
Timofei Iatsenko <ty@road.travel>
2024-06-18 10:52:21 +0200

    fix(experimental-extractor): fix various bugs (#1958)
    - update esbuild to 0.21.5
    - apply lingui macro on each file as esbuild plugin
    - remove buildExternalizeFilter in favour of `{packages: external}`

---
+++ packages/cli/src/extract-experimental/linguiEsbuildPlugin.ts
@@ -0,0 +1,56 @@
+import { transformAsync } from "@babel/core"
+import fs from "fs"
+import path from "path"
+import { Plugin } from "esbuild"
+import { babelRe, getBabelParserOptions } from "../api/extractors/babel"
+import linguiMacroPlugin, {
+  type LinguiPluginOpts,
+} from "@lingui/babel-plugin-lingui-macro"
+import { LinguiConfigNormalized } from "@lingui/conf"
+
+export const pluginLinguiMacro = (options: {
+  linguiConfig: LinguiConfigNormalized
+}): Plugin => ({
+  name: "linguiMacro",
+  setup(build) {
+    build.onLoad({ filter: babelRe, namespace: "" }, async (args) => {
+      const filename = path.relative(process.cwd(), args.path)
+
+      const contents = await fs.promises.readFile(args.path, "utf8")
+
+      const hasMacroRe = /from ["']@lingui(\/.+)?\/macro["']/g
+
+      if (!hasMacroRe.test(contents)) {
+        // let esbuild process file as usual
+        return undefined
+      }
+
+      const result = await transformAsync(contents, {
+        babelrc: false,
+        configFile: false,
+
+        filename: filename,
+
+        sourceMaps: "inline",
+        parserOpts: {
+          plugins: getBabelParserOptions(
+            filename,
+            options.linguiConfig.extractorParserOptions
+          ),
+        },
+
+        plugins: [
+          [
+            linguiMacroPlugin,
+            {
+              extract: true,
+              linguiConfig: options.linguiConfig,
+            } satisfies LinguiPluginOpts,
+          ],
+        ],
+      })
+
+      return { contents: result.code, loader: "jsx" }
+    })
+  },
+})
--- packages/cli/test/extractor-experimental/fixtures/pages/index.page.ts
+++ packages/cli/test/extractor-experimental/fixtures/pages/index.page.ts
@@ -1,4 +1,6 @@
 import { t } from "@lingui/core/macro"
+import { RED } from "../constants"
 
 const msg = t`index page message`
 console.log(msg)
+console.log(RED)
