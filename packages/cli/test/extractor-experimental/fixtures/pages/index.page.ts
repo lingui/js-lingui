81cd8b3c72a4bf349423e9ea38fc08c7e9496cbb
Timofei Iatsenko <ty@road.travel>
2024-07-08 09:54:05 +0200

    fix(experimental-extractor): change default loader to tsx
    Because we use babel to process only macro in esbuild plugin and left rest of the syntax (including typescript) as-is we have to specify correct loader for esbuild.
    
    I choosed tsx loader because it's a superset and do everything what js and ts and tsx loaders could do without relying on file extension

--- packages/cli/test/extractor-experimental/fixtures/pages/index.page.ts
+++ packages/cli/test/extractor-experimental/fixtures/pages/index.page.ts
@@ -1,6 +1,10 @@
 import { t } from "@lingui/core/macro"
 import { RED } from "../constants"
 
-const msg = t`index page message`
+const msg: string = t`index page message`
 console.log(msg)
 console.log(RED)
+
+function test(input: string): void {
+  console.log("Should support TS type annotation syntax")
+}
