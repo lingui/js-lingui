94f808d3b38676113fcefd36e34e928315c30366
Ben Grynhaus <me@bengr.dev>
2024-08-28 16:40:00 +0300

    remove `assert`

--- packages/react/src/format.test.tsx
+++ packages/react/src/format.test.tsx
@@ -1,6 +1,5 @@
-import * as React from "react"
 import { render } from "@testing-library/react"
-import assert from "node:assert"
+import * as React from "react"
 import { formatElements } from "./format"
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { mockConsole } from "@lingui/jest-mocks"
@@ -143,19 +142,13 @@ describe("formatElements", function () {
 
     expect(React.isValidElement(elements)).toBe(true)
 
-    assert.ok(
-      React.isValidElement<{
-        children: React.ReactElement[]
-      }>(elements)
-    )
-
-    const childElements = elements.props.children
+    const childElements = (
+      elements as React.ReactElement<{ children: React.ReactElement[] }>
+    ).props.children
     const childKeys = childElements.map((el) => el?.key).filter(Boolean)
 
-    assert.ok(
-      typeof childKeys[0] === "string" && typeof childKeys[1] === "string"
+    expect(cleanPrefix(childKeys[0] as string)).toBeLessThan(
+      cleanPrefix(childKeys[1] as string)
     )
-
-    expect(cleanPrefix(childKeys[0])).toBeLessThan(cleanPrefix(childKeys[1]))
   })
 })
