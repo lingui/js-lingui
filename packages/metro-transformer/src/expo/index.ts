c8374fe40581f97085864fe559d6897bcbb9ec14
Vojtech Novak <vonovak@gmail.com>
2024-09-29 22:26:48 +0200

    use require instead of import

--- packages/metro-transformer/src/expo/index.ts
+++ packages/metro-transformer/src/expo/index.ts
@@ -1,6 +1,5 @@
 import { createLinguiMetroTransformer } from "../metroTransformer"
 
-// @ts-expect-error the typings that are published with the package actually are not accurate
-import expoTransformer from "@expo/metro-config/babel-transformer"
+const expoTransformer = require("@expo/metro-config/babel-transformer")
 
 export const transform = createLinguiMetroTransformer(expoTransformer)
