9031279337f5fd0b5b0371b332ad1bf37bdc1efb
Andrii Bodnar <andrii.bodnar@crowdin.com>
2024-10-22 12:06:42 +0300

    docs: prettify sync & collaboration tools section (#2061)

--- website/sidebars.ts
+++ website/sidebars.ts
@@ -193,7 +193,23 @@ const sidebar = [
   {
     type: "category",
     label: "Sync & Collaboration Tools",
-    items: ["tools/introduction", "tools/crowdin", "tools/translation-io"],
+    items: [
+      {
+        type: "doc",
+        label: "Introduction",
+        id: "tools/introduction",
+      },
+      {
+        type: "doc",
+        label: "Crowdin",
+        id: "tools/crowdin",
+      },
+      {
+        type: "doc",
+        label: "Translation.io",
+        id: "tools/translation-io",
+      },
+    ],
   },
   {
     type: "category",
