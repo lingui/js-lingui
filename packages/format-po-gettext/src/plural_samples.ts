9e7b96521f5451a75710eb2d76d5a72a576d9629
Garik Khachatryan <garik@multimediallc.com>
2024-10-25 12:58:21 -0700

    dont need compiled functions

--- packages/format-po-gettext/src/plural_samples.ts
+++ packages/format-po-gettext/src/plural_samples.ts
@@ -61,102 +61,15 @@ export function createSamples(src: string): number[] {
       if (val.indexOf('~') !== -1) {
         result = result.concat(fillRange(val));
       } else {
-        // push test data as String
-        result.push(val);
-        // push test data as Number if the same
-        if (String(+val) === val) { result.push(+val); }
+        result.push(Number(val))
       }
     });
 
   return result;
 }
 
-// Create equation for single form rule
-function toSingleRule(str) {
-
-  return str
-    // replace modulus with shortcuts
-    .replace(/([nivwfte]) % (\d+)/g, '$1$2')
-    // replace ranges
-    .replace(/([nivwfte]\d*) (=|\!=) (\d+[.,][.,\d]+)/g, (match, v, cond, range) => {
-      // range = 5,8,9 (simple set)
-      if (range.indexOf('..') < 0 && range.indexOf(',') >= 0) {
-        if (cond === '=') {
-          return `IN([ ${range.split(',').join(', ')} ], ${v})`;
-        }
-        return `!IN([ ${range.split(',').join(', ')} ], ${v})`;
-      }
-      // range = 0..5 or 0..5,8..20 or 0..5,8
-      let conditions = range.split(',').map(interval => {
-        // simple value
-        if (interval.indexOf('..') < 0) return `${v} ${cond} ${interval}`;
-        // range
-        let [ start, end ] = interval.split('..');
-        if (cond === '=') return `B(${start}, ${end}, ${v})`;
-
-        return `!B(${start}, ${end}, ${v})`;
-      });
-
-      let joined;
-      if (conditions.length > 1) {
-        joined =  `(${conditions.join(cond === '=' ? ' || ' : ' && ')})`;
-      } else {
-        joined = conditions[0];
-      }
-      return joined;
-    })
-    .replace(/ = /g, ' === ')
-    .replace(/ != /g, ' !== ')
-    .replace(/ or /g, ' || ')
-    .replace(/ and /g, ' && ');
-}
-
-
-function createLocaleFn(rules) {
-
-  Object.keys(rules).forEach(r => {
-    if (FORMS.indexOf(r) < 0) { throw new Error("Don't know this form: "); }
-  });
-
-  // Make sure existing forms are ordered
-  // @ts-ignore
-  let forms = Object.keys(rules).sort((a, b) => FORMS.indexOf(a) > FORMS.indexOf(b));
-
-  if (forms.length === 1) return {};
-
-  let condition = '';
-
-  forms.forEach(function (form, idx) {
-    if (form === 'other') {
-      condition += idx;
-      return;
-    }
-    let rule = rules[form].split('@')[0].trim();
-    condition += `${toSingleRule(rule)} ? ${idx} : `;
-  });
-
-  let shortcuts = [ ...new Set(condition.match(/[nivwfte]\d+/g) || []) ] // unique
-    .map(sh => `${sh} = ${sh[0]} % ${sh.slice(1)}`)
-    .join(', ');
-
-  let pmax = Math.max(
-    ...('nivftwe'.split('').map((p, idx) => condition.indexOf(p) < 0 ? -1 : idx))
-  ) + 1;
-
-  let fn = `
-  function (${ 'nivftwe'.slice(0, pmax).split('').join(', ') }) {<% if (shortcuts) { %>
-    var ${ shortcuts };<% } %>
-    return ${ condition };
-  }
-  `
-  return {
-    c:   forms.map(f => FORMS.indexOf(f)),
-    cFn: fn
-  };
-}
-
 // Create fixtures for single locale rules
-function createLocaleTest(rules) {
+export function createLocaleTest(rules) {
   let result = {};
 
   Object.keys(rules).forEach(form => {
@@ -173,16 +86,15 @@ function createLocaleTest(rules) {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// Process all locales
-let compiled = {};
-const pluralRules = {}
+export function getCldrPluralSamples(): Record<string, Record<PluralForm, number[]>> {
+  const pluralRules = {}
 
-// Parse plural rules
-// Object.entries(cardinals.supplemental['plurals-type-cardinal']).forEach(([ loc, ruleset ]) => {
-//   let rules = renameKeys(ruleset);
+  // Parse plural rules
+  Object.entries(cardinals.supplemental['plurals-type-cardinal']).forEach(([ loc, ruleset ]) => {
+    let rules = renameKeys(ruleset);
 
-//   compiled[loc.toLowerCase()] = createLocaleFn(rules);
-//   pluralRules[loc.toLowerCase()] = createLocaleTest(rules);
-// });
+    pluralRules[loc.toLowerCase()] = createLocaleTest(rules);
+  });
 
-export default pluralRules
+  return pluralRules
+}
