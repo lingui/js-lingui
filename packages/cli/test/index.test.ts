222ff4db0d081e4e8f6b077bdf4d116602821617
Andrii Bodnar <andrii.bodnar@crowdin.com>
2024-11-28 12:33:04 +0200

    test: fix failed test after merging main

--- packages/cli/test/index.test.ts
+++ packages/cli/test/index.test.ts
@@ -334,8 +334,8 @@ describe("E2E Extractor Test", () => {
         ┌─────────────┬─────────────┬─────────┐
         │ Language    │ Total count │ Missing │
         ├─────────────┼─────────────┼─────────┤
-        │ en (source) │      8      │    -    │
-        │ pl          │      8      │    8    │
+        │ en (source) │     10      │    -    │
+        │ pl          │     10      │   10    │
         └─────────────┴─────────────┴─────────┘
 
         (Use "yarn extract" to update catalogs with new messages.)
