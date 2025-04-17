216551ca4afb5d44a77d40f0e0ed7af105a048e0
Vojtech Novak <vonovak@gmail.com>
2025-04-17 19:40:04 +0200

    fix: update metro config

--- examples/react-native/metro.config.js
+++ examples/react-native/metro.config.js
@@ -6,6 +6,16 @@ const { resolver } = config;
 
 config.transformer.babelTransformerPath = require.resolve("@lingui/metro-transformer/expo");
 config.resolver.sourceExts = [...resolver.sourceExts, "po", "pot"]
+// unstable_enablePackageExports is true by default in React Native 0.79.0 and above
 config.resolver.unstable_enablePackageExports = true
+config.resolver.resolveRequest = (context, moduleImport, platform) => {
+  switch (moduleImport) {
+    case '@lingui/react':
+      return { type: 'sourceFile', filePath: require.resolve('@lingui/react') };
+
+    default:
+      return context.resolveRequest(context, moduleImport, platform);
+  }
+};
 
 module.exports = config;
