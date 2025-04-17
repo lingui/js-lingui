dfada1fd6d46355d72c83897d1ce52ac1451a72b
Vojtech Novak <vonovak@gmail.com>
2025-04-17 09:13:17 +0200

    chore: update react-native example to Expo SDK 53

--- examples/react-native/metro.config.js
+++ examples/react-native/metro.config.js
@@ -2,15 +2,10 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
-const { transformer, resolver } = config;
+const { resolver } = config;
 
-config.transformer = {
-  ...transformer,
-  babelTransformerPath: require.resolve("@lingui/metro-transformer/expo"),
-};
-config.resolver = {
-  ...resolver,
-  sourceExts: [...resolver.sourceExts, "po", "pot"],
-};
+config.transformer.babelTransformerPath = require.resolve("@lingui/metro-transformer/expo");
+config.resolver.sourceExts = [...resolver.sourceExts, "po", "pot"]
+config.resolver.unstable_enablePackageExports = true
 
 module.exports = config;
