e43179164de52e9af0d2d9899141ceeb4257c2a2
Simon Depelchin <dep.simon@gmail.com>
2025-06-18 09:16:24 +0200

    Devinxi Tanstack Start example

---
+++ examples/tanstack-start/vite.config.ts
@@ -0,0 +1,30 @@
+import { lingui } from "@lingui/vite-plugin"
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths"
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext'
+  },
+  plugins: [
+    lingui(),
+    tailwindcss(),
+    tsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tanstackStart({
+      tsr: {
+      },
+      react: {
+        babel: {
+          plugins: ["@lingui/babel-plugin-lingui-macro"],
+        },
+      },
+    })
+  ],
+})
