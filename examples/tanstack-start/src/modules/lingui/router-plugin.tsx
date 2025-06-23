4ea33d08ec4779895b770c672a1b83e9368f777b
Simon Depelchin <dep.simon@gmail.com>
2025-06-23 12:11:09 +0200

    examples(tanstack-start): hydrate catalog from server

---
+++ examples/tanstack-start/src/modules/lingui/router-plugin.tsx
@@ -0,0 +1,63 @@
+import { Fragment, type PropsWithChildren } from 'react'
+import { I18nProvider } from "@lingui/react"
+import type { AnyRouter } from '@tanstack/react-router'
+import { setupI18n, type I18n } from '@lingui/core'
+
+type AdditionalOptions = {
+  WrapProvider?: (props: { children: any }) => React.JSX.Element
+}
+
+export type ValidateRouter<TRouter extends AnyRouter> =
+  NonNullable<TRouter['options']['context']> extends {
+    i18n: I18n
+  }
+    ? TRouter
+    : never
+
+export function routerWithLingui<TRouter extends AnyRouter>(
+  router: ValidateRouter<TRouter>,
+  i18n: I18n,
+  additionalOpts?: AdditionalOptions,
+): TRouter {
+  const ogOptions = router.options
+
+  router.options = {
+    ...router.options,
+    dehydrate: () => {
+      return {
+        ...ogOptions.dehydrate?.(),
+        // When critical data is dehydrated, we also dehydrate the i18n messages
+        dehydratedI18n: {
+            locale: i18n.locale,
+            messages: i18n.messages,
+        },
+      }
+    },
+    hydrate: (dehydrated: any) => {
+      ogOptions.hydrate?.(dehydrated)
+      // On the client, hydrate the i18n catalog with the dehydrated data
+      i18n.loadAndActivate({
+        locale: dehydrated.dehydratedI18n.locale,
+        messages: dehydrated.dehydratedI18n.messages,
+      })
+    },
+    context: {
+      ...ogOptions.context,
+      // Pass the query client to the context, so we can access it in loaders
+      i18n,
+    },
+    // Wrap the app in a I18nProvider
+    Wrap: ({ children }: PropsWithChildren) => {
+      const OuterWrapper = additionalOpts?.WrapProvider || Fragment
+      const OGWrap = ogOptions.Wrap || Fragment
+
+      return <OuterWrapper>
+          <I18nProvider i18n={i18n}>
+            <OGWrap>{children}</OGWrap>
+          </I18nProvider>
+        </OuterWrapper>
+    },
+  }
+
+  return router
+}
--- examples/tanstack-start/src/routes/deferred.tsx
+++ examples/tanstack-start/src/routes/deferred.tsx
@@ -4,10 +4,10 @@ import { Suspense, useState } from "react"
 import { personServerFn, slowServerFn } from "~/functions/deferred"
 
 export const Route = createFileRoute("/deferred")({
-  loader: async () => {
+  loader: async ({ context }) => {
     return {
       deferredStuff: new Promise<string>((r) =>
-        setTimeout(() => r("Hello deferred!"), 2000)
+        setTimeout(() => r(context.i18n.t("Hello deferred!")), 2000)
       ),
       deferredPerson: slowServerFn({ data: "Tanner Linsley" }),
       person: await personServerFn({ data: "John Doe" }),
