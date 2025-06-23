71d0b21aaaf514f96887b6cfb3e2dd6fca9bfd25
Simon Depelchin <dep.simon@gmail.com>
2025-06-23 13:50:45 +0200

    examples(tanstack-start): prefer using the `msg` macro

--- examples/tanstack-start/src/routes/deferred.tsx
+++ examples/tanstack-start/src/routes/deferred.tsx
@@ -1,3 +1,4 @@
+import { msg } from "@lingui/core/macro"
 import { Trans } from "@lingui/react/macro"
 import { Await, createFileRoute } from "@tanstack/react-router"
 import { Suspense, useState } from "react"
@@ -7,7 +8,7 @@ export const Route = createFileRoute("/deferred")({
   loader: async ({ context }) => {
     return {
       deferredStuff: new Promise<string>((r) =>
-        setTimeout(() => r(context.i18n.t("Hello deferred!")), 2000)
+        setTimeout(() => r(context.i18n._(msg `Hello deferred!`)), 2000)
       ),
       deferredPerson: slowServerFn({ data: "Tanner Linsley" }),
       person: await personServerFn({ data: "John Doe" }),
