891290f1695adfa4d6a27f78d974bbbc6145f8a3
Simon Depelchin <dep.simon@gmail.com>
2025-06-23 12:11:09 +0200

    examples(tanstack-start): hydrate catalog from server

--- examples/tanstack-start/src/client.tsx
+++ examples/tanstack-start/src/client.tsx
@@ -1,14 +1,12 @@
 import { setupI18n } from "@lingui/core"
 import { hydrateRoot } from "react-dom/client"
 import { StartClient } from "@tanstack/react-start"
-import { dynamicActivate } from "./modules/lingui/i18n"
 
 import { createRouter } from "./router"
 import { startTransition, StrictMode } from "react"
 
 // The lang should be set by the server
 const i18n = setupI18n({})
-await dynamicActivate(i18n, document.documentElement.lang)
 
 const router = createRouter({ i18n })
 
--- examples/tanstack-start/src/router.tsx
+++ examples/tanstack-start/src/router.tsx
@@ -1,28 +1,26 @@
 import type { I18n } from "@lingui/core"
 import { I18nProvider } from "@lingui/react"
-import { createRouter as createTanStackRouter } from "@tanstack/react-router"
+import { createRouter as createTanStackRouter, type AnyRouter } from "@tanstack/react-router"
 import { type PropsWithChildren } from "react"
 import { routeTree } from "./routeTree.gen"
 import { DefaultCatchBoundary } from "./components/DefaultCatchBoundary"
 import { NotFound } from "./components/NotFound"
+import { routerWithLingui } from "./modules/lingui/router-plugin"
 
 export interface AppContext {
   i18n: I18n
 }
 
 export function createRouter({ i18n }: { i18n: I18n }) {
-  const router = createTanStackRouter({
+  const router = routerWithLingui(createTanStackRouter({
     routeTree,
     context: {
       i18n,
     },
     defaultErrorComponent: DefaultCatchBoundary,
     defaultNotFoundComponent: () => <NotFound />,
-    scrollRestoration: true,
-    Wrap: ({ children }: PropsWithChildren) => {
-      return <I18nProvider i18n={i18n}>{children}</I18nProvider>
-    },
-  })
+    scrollRestoration: true
+  }), i18n)
 
   return router
 }
