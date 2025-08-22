921f1543c9b26873f379037a03b7791c6a9d3eec
Timofei Iatsenko <timiatsenko@gmail.com>
2025-08-22 11:10:35 +0200

    fix build

--- packages/cli/src/api/extractWorkerPool.ts
+++ packages/cli/src/api/extractWorkerPool.ts
@@ -3,6 +3,7 @@ import type { ExtractWorkerFunction } from "../workers/extractWorker"
 
 export type ExtractWorkerPool = ReturnType<typeof createExtractWorkerPool>
 
+/** @internal */
 export function createExtractWorkerPool() {
   return Pool(() =>
     spawn<ExtractWorkerFunction>(new Worker("../workers/extractWorker"))
