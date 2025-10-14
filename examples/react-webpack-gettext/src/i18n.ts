d941835c2c394eba48a081ce1c979e6a6962ddcd
Garik Khachatryan <garik@multimediallc.com>
2025-10-14 14:33:38 -0700

    like exactly the same

--- examples/react-webpack-gettext/src/i18n.ts
+++ examples/react-webpack-gettext/src/i18n.ts
@@ -1,17 +1,17 @@
-import { i18n } from "@lingui/core"
+import { i18n } from "@lingui/core";
 
 export const locales = {
   en: "English",
   cs: "Česky",
-}
-export const defaultLocale = "cs"
+};
+export const defaultLocale = "en";
 
-export function dynamicActivate(locale: string) {
-    fetch(`locales/${locale}.json`).then(response => response.json()).then(data => {
-        console.log(`Loaded translations for ${locale}:`, data);
-        i18n.load(locale, data)
-        i18n.activate(locale)
-    }).catch(error => {
-        console.error('Failed to load translations:', error);
-    });
+/**
+ * We do a dynamic import of just the catalog that we need
+ * @param locale any locale string
+ */
+export async function dynamicActivate(locale: string) {
+  const { messages } = await import(`./locales/${locale}.json`);
+  i18n.load(locale, messages);
+  i18n.activate(locale);
 }
