622cc1a3e62d7d48731b82c57bd39f071fa648e0
Timofei <timiatsenko@gmail.com>
2025-11-20 08:59:02 +0100

    setup coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,19 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   test: {
+    coverage: {
+      provider: "v8",
+      include: ["packages/**/*.{ts,tsx}"],
+      exclude: [
+        "**/node_modules/**",
+        "**/build/**",
+        "**/fixtures/**",
+        "**/test/**",
+        "**/__typetests__/**",
+        "**/*.config.ts",
+        "**/*.d.ts",
+      ],
+    },
     projects: ["packages/*"],
   },
 })
