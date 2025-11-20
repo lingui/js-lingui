57b3d9886e8621d76e409acb0352a2d83c441450
Timofei <timiatsenko@gmail.com>
2025-11-20 08:58:55 +0100

    vitest plugin test fixes

--- packages/vite-plugin/test/vite-plugin.test.ts
+++ packages/vite-plugin/test/vite-plugin.test.ts
@@ -4,15 +4,17 @@ import os from "os"
 import { platform } from "node:os"
 import { describe } from "vitest"
 import { build, createLogger } from "vite"
+import lingui, { LinguiPluginOpts } from "../src"
+import macrosPlugin from "vite-plugin-babel-macros"
 
 describe.skipIf(platform() === "win32")("vite-plugin", () => {
   it("should return compiled catalog", async () => {
-    const { mod } = await runVite(`po-format/vite.config.ts`)
+    const { mod } = await runVite(`po-format`)
     expect((await mod.load()).messages).toMatchSnapshot()
   })
 
   it("should return compiled catalog json", async () => {
-    const { mod } = await runVite(`json-format/vite.config.ts`)
+    const { mod } = await runVite(`json-format`)
 
     expect((await mod.load()).messages).toMatchSnapshot()
   })
@@ -20,7 +22,7 @@ describe.skipIf(platform() === "win32")("vite-plugin", () => {
   it("should report error when macro used without a plugin", async () => {
     expect.assertions(1)
     try {
-      await runVite(`no-macro-error/vite.config.ts`)
+      await runVite(`no-macro-error`)
     } catch (e) {
       expect(e.message).toContain(
         'The macro you imported from "@lingui/core/macro" is being executed outside the context of compilation.'
@@ -28,14 +30,16 @@ describe.skipIf(platform() === "win32")("vite-plugin", () => {
     }
   })
   it("should not report error when macro correctly used", async () => {
-    const { mod } = await runVite(`macro-usage/vite.config.ts`)
+    const { mod } = await runVite(`macro-usage`, {}, true)
     expect(await mod.load()).toMatchSnapshot()
   })
 
   it("should report missing error when failOnMissing = true", async () => {
     expect.assertions(1)
     try {
-      await runVite(`fail-on-missing/vite.config.ts`)
+      await runVite(`fail-on-missing`, {
+        failOnMissing: true,
+      })
     } catch (e) {
       expect(e.message).toContain("Missing 1 translation(s):")
     }
@@ -43,23 +47,27 @@ describe.skipIf(platform() === "win32")("vite-plugin", () => {
 
   it("should NOT report missing messages for pseudo locale when failOnMissing = true", async () => {
     await expect(
-      runVite(`fail-on-missing-pseudo/vite.config.ts`)
+      runVite(`fail-on-missing-pseudo`, {
+        failOnMissing: true,
+      })
     ).resolves.toBeTruthy()
   })
 
   it("Should fail build if there are message compilation errors when failOnCompileError = true", async () => {
     expect.assertions(1)
     try {
-      await runVite(`fail-on-compile-errors/vite.config.ts`)
+      await runVite(`fail-on-compile-errors`, {
+        failOnCompileError: true,
+      })
     } catch (e) {
       expect(e.message).toContain("Compilation error for 2 translation(s)")
     }
   })
 
   it("Should NOT fail build if there are message compilation errors when failOnCompileError = false", async () => {
-    const res = await runVite(
-      `fail-on-compile-errors/failOnCompileErrorFalse.vite.config.ts`
-    )
+    const res = await runVite(`fail-on-compile-errors`, {
+      failOnCompileError: false,
+    })
 
     expect(res.warn).toContain("Compilation error for 2 translation(s)")
   })
@@ -67,7 +75,7 @@ describe.skipIf(platform() === "win32")("vite-plugin", () => {
   it("should report error when @lingui/macro is dynamically imported", async () => {
     expect.assertions(1)
     try {
-      await runVite(`dynamic-macro-error/vite.config.ts`)
+      await runVite(`dynamic-macro-error`, {}, true)
     } catch (e) {
       expect(e.message).toContain(
         'The macro you imported from "@lingui/core/macro" cannot be dynamically imported.'
@@ -76,10 +84,14 @@ describe.skipIf(platform() === "win32")("vite-plugin", () => {
   })
 })
 
-async function runVite(configPath: string) {
+async function runVite(
+  fixturesPath: string,
+  pluginConfig: LinguiPluginOpts = {},
+  useMacroPlugin = false
+) {
   const oldCwd = process.cwd()
-  //
-  process.chdir(path.join(import.meta.dirname, path.dirname(configPath)))
+  const cwd = path.join(import.meta.dirname, fixturesPath)
+  process.chdir(cwd)
 
   const outDir = mkdtempSync(
     path.join(os.tmpdir(), `lingui-test-${process.pid}`)
@@ -98,14 +110,35 @@ async function runVite(configPath: string) {
     warnMsgs.push(msg)
   }
 
+  logger.error = (msg, options) => {
+    console.log(msg)
+    warnMsgs.push(msg)
+  }
+
   try {
     await build({
-      configFile: path.resolve(import.meta.dirname, configPath),
       customLogger: logger,
       build: {
         emptyOutDir: true,
+        lib: {
+          entry: path.resolve(
+            import.meta.dirname,
+            fixturesPath,
+            "entrypoint.js"
+          ),
+          fileName: "bundle",
+          formats: ["es"],
+        },
         outDir,
       },
+      plugins: [
+        lingui({
+          cwd,
+          ...pluginConfig,
+        }),
+
+        useMacroPlugin ? macrosPlugin() : [],
+      ],
     })
   } finally {
     process.chdir(oldCwd)
