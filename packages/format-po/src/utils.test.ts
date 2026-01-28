20919559208fb49f6cef1c433eeee911e0de4f1c
Timofei <timiatsenko@gmail.com>
2026-01-28 13:11:53 +0100

    cleanup

--- packages/format-po/src/utils.test.ts
+++ packages/format-po/src/utils.test.ts
@@ -12,7 +12,7 @@ describe("normalizePlaceholderValue", () => {
 })
 
 describe("formatPotCreationDate", () => {
-  it('Should format date in `xgettext` compatible format "%Y-%m-%d %H:%M%z"', (input) => {
+  it('Should format date in `xgettext` compatible format "%Y-%m-%d %H:%M%z"', () => {
     expect(
       formatPotCreationDate(new Date("2018-08-27T10:00Z")),
     ).toMatchInlineSnapshot(`"2018-08-27 10:00+0000"`)
