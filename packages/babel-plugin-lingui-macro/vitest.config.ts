7f98167f510550f9401297e123fb3a218e411996
Yaroslav Slepukhin <YaroslavSlepukhin@gmail.com>
2026-02-04 14:58:35 +0100

    chore: explicit "type" field in package.json and alignment for "exports" (#2430)

--- packages/babel-plugin-lingui-macro/vitest.config.ts
+++ packages/babel-plugin-lingui-macro/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   test: {
-    snapshotSerializers: ["./.vitest/code-serializer.js"],
+    snapshotSerializers: ["./.vitest/serializer.ts"],
     globals: true,
   },
 })
