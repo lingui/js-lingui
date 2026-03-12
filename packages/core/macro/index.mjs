91355148881b79ce7da2ae203fec09656f673454
Timofei <1586852+timofei-iatsenko@users.noreply.github.com>
2026-03-12 14:32:53 +0100

    fix(macro): add shims for macro executed in nodejs without transpilation

--- packages/core/macro/index.mjs
+++ packages/core/macro/index.mjs
@@ -6,3 +6,19 @@ const { createMacro } = babelPluginMacros
 export default createMacro(macro, {
   configName: "lingui",
 })
+
+// the following shims for the case when this import used by nodejs code without transpilation
+function printError() {
+  throw new Error(
+    `The macro you imported from "@lingui/core/macro" is being executed outside the context of compilation. \n` +
+      `This indicates that you don't configured correctly one of the "babel-plugin-macros" / "@lingui/swc-plugin" / "babel-plugin-lingui-macro"`,
+  )
+}
+
+export const t = printError
+export const msg = printError
+export const ph = printError
+export const defineMessage = printError
+export const plural = printError
+export const select = printError
+export const selectOrdinal = printError
--- packages/react/macro/index.mjs
+++ packages/react/macro/index.mjs
@@ -6,3 +6,17 @@ const { createMacro } = babelPluginMacros
 export default createMacro(macro, {
   configName: "lingui",
 })
+
+// the following shims for the case when this import used by nodejs code without transpilation
+function printError() {
+  throw new Error(
+    `The macro you imported from "@lingui/react/macro" is being executed outside the context of compilation. \n` +
+      `This indicates that you don't configured correctly one of the "babel-plugin-macros" / "@lingui/swc-plugin" / "babel-plugin-lingui-macro"`,
+  )
+}
+
+export const Trans = printError
+export const Plural = printError
+export const Select = printError
+export const SelectOrdinal = printError
+export const useLingui = printError
