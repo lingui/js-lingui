7fef1dc0b5211755e5541fad4826dcd5daf004a6
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2026-04-13 17:17:17 +0200

    docs: update rspack example, add @lingui/loader to example

--- examples/rspack/src/main.tsx
+++ examples/rspack/src/main.tsx
@@ -1,30 +1,26 @@
 import React from "react";
+import { i18n, } from "@lingui/core";
 import { createRoot } from "react-dom/client";
 
-import { i18n } from "@lingui/core";
 import { I18nProvider } from "@lingui/react";
-import { messages as enMessages } from "./locales/en/messages";
-import { messages as frMessages } from "./locales/fr/messages";
 
 import Inbox from "./Inbox";
+import { dynamicActivate } from "./i18n";
+import Locale from "./locales";
 
-i18n.load({
-  "en": enMessages,
-  "fr": frMessages,
-});
-
-i18n.activate("en");
-
-const App = () => (
-  <I18nProvider i18n={i18n}>
-    <Inbox />
-  </I18nProvider>
-);
-
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+
+void dynamicActivate(i18n, Locale.ENGLISH)
+  .then(() => {
+    root.render(
+      <React.StrictMode>
+        <I18nProvider i18n={i18n}>
+          <Inbox />
+        </I18nProvider>
+      </React.StrictMode>,
+    );
+  })
+  .catch((error) => {
+    console.error("Failed to load initial catalog.", error);
+  });
