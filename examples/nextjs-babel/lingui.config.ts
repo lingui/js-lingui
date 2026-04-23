7421041c2aebd6d80eb11a1da8167c7b8223e06b
Timofei <1586852+timofei-iatsenko@users.noreply.github.com>
2026-04-23 13:28:52 +0200

    ęxamples: update nextjs-babel to next@16 lingui@6

--- examples/nextjs-babel/lingui.config.ts
+++ examples/nextjs-babel/lingui.config.ts
@@ -1,8 +1,8 @@
-const nextConfig = require("./next.config")
+import {defineConfig} from "@lingui/cli"
+import nextConfig from "./next.config.js"
 
-/** @type {import('@lingui/conf').LinguiConfig} */
-module.exports = {
-  locales: nextConfig.i18n.locales,
+export default defineConfig({
+  locales: nextConfig.i18n.locales as string[],
   pseudoLocale: "pseudo",
   sourceLocale: nextConfig.i18n.defaultLocale,
   fallbackLocales: {
@@ -14,4 +14,4 @@ module.exports = {
       include: ["src/"],
     },
   ],
-}
+})
