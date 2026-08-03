da1eefde07af18d70fe3831c26b8a7d9661551cc
Matthieu Sieben <matthieusieben@users.noreply.github.com>
2026-08-03 13:17:39 +0200

    Export types from @lingui/conf in index.ts
    Fixes the following TypeScript error:
    
    > The inferred type of 'default' cannot be named without a reference to 'LinguiConfig' from '.pnpm/@lingui+conf@6.6.0/node_modules/@lingui/conf'. This is likely not portable. A type annotation is necessary.

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,2 @@
-export { defineConfig } from "@lingui/conf"
+// Re-export both the `defineConfig` function and the types it depends on
+export { defineConfig, type LinguiConfig } from "@lingui/conf"
